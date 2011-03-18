66c770b1da68ebc1d893ba7599e13289c36b03a4
unknown <a_naumenko@.IV>
2011-03-18 19:02:57 +0200

    update

--- cpp/ConvexHull.cpp
+++ cpp/ConvexHull.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-typedef pair<double, double> point;
+typedef pair<long, long> point;
 
 bool cw(const point &a, const point &b, const point &c) {
     return (b.first - a.first) * (c.second - a.second) - (b.second - a.second) * (c.first - a.first) < 0;
@@ -13,16 +13,16 @@ vector<point> convexHull(vector<point> p) {
     int n = p.size();
     if (n <= 1)
         return p;
-    int k = 0;
     sort(p.begin(), p.end());
+    int cnt = 0;
     vector<point> q(n * 2);
-    for (int i = 0; i < n; q[k++] = p[i++])
-        for (; k >= 2 && !cw(q[k - 2], q[k - 1], p[i]); --k)
+    for (int i = 0; i < n; q[cnt++] = p[i++])
+        for (; cnt >= 2 && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
             ;
-    for (int i = n - 2, t = k; i >= 0; q[k++] = p[i--])
-        for (; k > t && !cw(q[k - 2], q[k - 1], p[i]); --k)
+    for (int i = n - 2, t = cnt; i >= 0; q[cnt++] = p[i--])
+        for (; cnt > t && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
             ;
-    q.resize(k - 1 - (q[0] == q[1]));
+    q.resize(cnt - 1 - (q[0] == q[1]));
     return q;
 }
 
