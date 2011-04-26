8ad7152ab4bbab262943341eaf400841d0b2b40c
indy256 <indy256@gmail.com>
2011-04-26 08:31:21 +0300

    update

--- cpp/DisjointSets.cpp
+++ cpp/DisjointSets.cpp
@@ -3,39 +3,38 @@
 using namespace std;
 
 const int maxn = 200000;
-int rank[maxn];
+int Rank[maxn];
 int p[maxn];
 int n;
 
 void init(int _n) {
-    n = _n;
-    fill(rank, rank + n, 0);
-    for (int i = 0; i < n; i++)
-        p[i] = i;
+	n = _n;
+	fill(Rank, Rank + n, 0);
+	for (int i = 0; i < n; i++) p[i] = i;
 }
 
 int root(int x) {
-    if (p[x] != x)
-        p[x] = root(p[x]);
-    return p[x];
+	if (x == p[x]) return x;
+	return p[x] = root(p[x]);
 }
 
 void unite(int a, int b) {
-    a = root(a);
-    b = root(b);
-    if (rank[a] < rank[b]) {
-        p[a] = b;
-    } else {
-        p[b] = a;
-        if (rank[a] == rank[b])
-            ++rank[a];
-    }
+	a = root(a);
+	b = root(b);
+	if (a == b)return;
+	if (Rank[a] < Rank[b]) {
+		p[a] = b;
+	} else {
+		p[b] = a;
+		if (Rank[a] == Rank[b])
+			++Rank[a];
+	}
 }
 
 int main() {
-    init(3);
-    unite(0, 2);
-    cout << root(0) << endl;
-    cout << root(1) << endl;
-    cout << root(2) << endl;
+	init(3);
+	unite(0, 2);
+	cout << root(0) << endl;
+	cout << root(1) << endl;
+	cout << root(2) << endl;
 }
--- cpp/FenwickTree.cpp
+++ cpp/FenwickTree.cpp
@@ -5,38 +5,38 @@ const int maxn = 200000;
 int t[maxn];
 
 void add(int i, int value) {
-    for (; i < maxn; i |= i + 1 /* i += (i + 1) & -(i + 1) */)
-        t[i] += value;
+	for (; i < maxn; i |= i + 1 /* i += (i + 1) & -(i + 1) */)
+		t[i] += value;
 }
 
 int sum(int i) {
-    int res = 0;
-    for (; i >= 0; i -= -~i & ~i /* i -= (i + 1) & -(i + 1) */)
-        res += t[i];
-    return res;
+	int res = 0;
+	for (; i >= 0; i -= -~i & ~i /* i -= (i + 1) & -(i + 1) */)
+		res += t[i];
+	return res;
 }
 
 // sum[a,b]
 int sum(int a, int b) {
-    return sum(b) - sum(a - 1);
+	return sum(b) - sum(a - 1);
 }
 
 int get(int i) {
-    return sum(i, i);
+	return sum(i, i);
 }
 
 void set(int i, int value) {
-    add(i, -get(i) + value);
+	add(i, -get(i) + value);
 }
 
 // Usage example
 int main() {
-    set(0, 4);
-    set(1, 5);
-    set(2, 5);
-    add(2, 5);
+	set(0, 4);
+	set(1, 5);
+	set(2, 5);
+	add(2, 5);
 
-    cout << (4 == get(0)) << endl;
-    cout << (19 == sum(0, 2)) << endl;
-    cout << (15 == sum(1, 2)) << endl;
+	cout << (4 == get(0)) << endl;
+	cout << (19 == sum(0, 2)) << endl;
+	cout << (15 == sum(1, 2)) << endl;
 }
