70638194a6407e7cd3eb27362f6a12b67b8fecaf
unknown <a_naumenko@.IV>
2011-09-09 14:58:32 +0300

    update

--- java/src/PrimesGenerator.java
+++ java/src/PrimesGenerator.java
@@ -3,23 +3,40 @@ import java.util.*;
 public class PrimesGenerator {
 
 	public static int[] getPrimes(int n) {
-		if (n <= 1)
-			return new int[0];
 		boolean[] prime = new boolean[n + 1];
 		Arrays.fill(prime, 2, n + 1, true);
-		int[] primes = new int[n + 1];
-		int cnt = 0;
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i * i <= n; i++) {
 			if (prime[i]) {
-				for (int j = i * i; j <= n; j += i)
-					if (prime[j])
-						prime[j] = false;
-				primes[cnt++] = i;
+				for (int j = i * i; j <= n; j += i) {
+					prime[j] = false;
+				}
 			}
 		}
-		int[] res = new int[cnt];
-		System.arraycopy(primes, 0, res, 0, cnt);
-		return res;
+		int[] primes = new int[n + 1];
+		int cnt = 0;
+		for (int i = 0; i < prime.length; i++)
+			if (prime[i])
+				primes[cnt++] = i;
+
+		return Arrays.copyOf(primes, cnt);
+	}
+
+	public static int[] getPrimesLinear(int n) {
+		int[] lp = new int[n + 1];
+		int[] primes = new int[n + 1];
+		int cnt = 0;
+
+		for (int i = 2; i <= n; ++i) {
+			if (lp[i] == 0) {
+				lp[i] = i;
+				primes[cnt++] = i;
+			}
+			for (int j = 0; j < cnt && primes[j] <= lp[i] && i * primes[j] <= n; ++j) {
+				lp[i * primes[j]] = primes[j];
+			}
+		}
+
+		return Arrays.copyOf(primes, cnt);
 	}
 
 	public static boolean isPrime(long n) {
@@ -35,9 +52,8 @@ public class PrimesGenerator {
 
 	public static int[] numberOfDivisors(int n) {
 		int[] am = new int[n + 1];
-		for (int i = 0; i <= n; ++i)
+		for (int i = 2; i <= n; ++i)
 			am[i] = 1;
-		am[1] = 0;
 		for (int i = 2; i * i <= n; ++i)
 			if (am[i] == 1) {
 				for (int j = i * i, k = i; j <= n; j += i, ++k)
@@ -48,20 +64,15 @@ public class PrimesGenerator {
 
 	// Usage example
 	public static void main(String[] args) {
-		String primes1 = "";
-		String primes2 = "";
 		int n = 31;
-		for (int p : getPrimes(n))
-			primes1 += p + " ";
 
-		for (int i = 0; i <= n; i++)
-			if (isPrime(i))
-				primes2 += i + " ";
+		int[] primes1 = getPrimes(n);
+		int[] primes2 = getPrimesLinear(n);
 
-		System.out.println(primes1);
-		System.out.println(primes2);
-		System.out.println(primes1.equals(primes2));
-		
+		System.out.println(Arrays.toString(primes1));
+		System.out.println(Arrays.toString(primes2));
+		System.out.println(Arrays.equals(primes1, primes2));
+
 		System.out.println(Arrays.toString(numberOfDivisors(n)));
 	}
 }
