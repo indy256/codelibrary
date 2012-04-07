a7eb5c00ac93a2a72a549a948ffcba8a62c166cb
indy256 <indy256@gmail.com>
2012-04-07 20:51:58 +0300

    update

--- java/src/SimulatedAnnealing.java
+++ java/src/SimulatedAnnealing.java
@@ -1,69 +1,52 @@
 import java.awt.*;
-import java.io.PrintWriter;
 import java.util.*;
 import javax.swing.*;
 
 public class SimulatedAnnealing extends JFrame {
-	static Random rnd = new Random();
-	JPanel panel;
-	int[] x;
-	int[] y;
-	int[] p;
+	Random rnd = new Random();
+	int n = rnd.nextInt(300) + 150;
 
-	static int[] getRandomCombination(int n, int m) {
-		int[] res = new int[n];
+	double[] x = new double[n];
+	double[] y = new double[n];
+	int[] p = new int[n];
+	{
 		for (int i = 0; i < n; i++) {
-			res[i] = i;
+			x[i] = rnd.nextDouble();
+			y[i] = rnd.nextDouble();
+			p[i] = i;
 		}
-		for (int i = 0; i < m; i++) {
-			int j = n - 1 - rnd.nextInt(n - i);
-			int t = res[i];
-			res[i] = res[j];
-			res[j] = t;
-		}
-		return Arrays.copyOf(res, m);
 	}
 
-	static int[] getRandomPermutation(int n) {
-		int[] res = new int[n];
-		for (int i = 0; i < n; i++) {
-			res[i] = i;
+	int[] neighbour(int[] state, double curTemp) {
+		int n = state.length;
+		int i = 0;
+		int j = 0;
+		do {
+			i = rnd.nextInt(n);
+			j = rnd.nextInt(n);
+		} while (Math.abs(i - j) > n * curTemp);
+		
+		int[] newState = state.clone();
+		int sign = i - j;
+		// reverse order from i to j
+		while (sign * (i - j) > 0) {
+			int t = newState[i];
+			newState[i] = newState[j];
+			newState[j] = t;
+			i = (i + 1) % n;
+			j = (j - 1 + n) % n;
 		}
-		for (int i = res.length - 1; i > 0; i--) {
-			int j = rnd.nextInt(i + 1);
-			int t = res[i];
-			res[i] = res[j];
-			res[j] = t;
-		}
-		return res;
+		return newState;
 	}
 
-	int[] neighbour(int[] state, int changes) {
-		int[] p = getRandomCombination(state.length, changes);
-		int[] r = getRandomPermutation(changes);
-		int[] res = state.clone();
-		for (int i = 0; i < changes; i++) {
-			res[p[i]] = state[p[r[i]]];
-		}
-		return res;
-	}
-
-	double eval(int[] state) {
-		double res = 0;
-		for (int i = 0, j = state.length - 1; i < state.length; j = i++) {
-			int dx = x[state[i]] - x[state[j]];
-			int dy = y[state[i]] - y[state[j]];
-			res += Math.sqrt(dx * dx + dy * dy);
-		}
-		return res;
-	}
-
-	static double Prob(double curEnergy, double newEnergy, double curTemp) {
+	double Prob(double curEnergy, double newEnergy, double curTemp) {
 		double diff = newEnergy - curEnergy;
 		if (diff < 0) {
 			return 1;
 		} else {
-			return 0 * Math.exp(-diff / curTemp);
+			int magicValue = 100000;
+			double probability = Math.exp(-diff * magicValue / n / curTemp);
+			return probability;
 		}
 	}
 
@@ -75,10 +58,9 @@ public class SimulatedAnnealing extends JFrame {
 		double curEnergy = eval(curState);
 		int[] bestState = curState;
 		double bestEnergy = curEnergy;
-		double maxTemp = 10000;
-		for (double curTemp = maxTemp; curTemp > 0; curTemp -= 0.01) {
-			int changes = Math.max(2, (int) (curTemp / maxTemp * p.length));
-			int[] newState = neighbour(curState, changes);
+		double temperatureStep = 1e-7;
+		for (double curTemp = 1; curTemp > 0; curTemp -= temperatureStep) {
+			int[] newState = neighbour(curState, curTemp);
 			double newEnergy = eval(newState);
 
 			if (Prob(curEnergy, newEnergy, curTemp) > rnd.nextDouble()) {
@@ -96,46 +78,38 @@ public class SimulatedAnnealing extends JFrame {
 		}
 	}
 
-	public SimulatedAnnealing() {
-		final int n = rnd.nextInt(20) + 50;
-		x = new int[n];
-		y = new int[n];
-		p = new int[n];
-		final int max = 400;
-
-		for (int i = 0; i < n; i++) {
-			x[i] = rnd.nextInt(max);
-			y[i] = rnd.nextInt(max);
-			p[i] = i;
+	double eval(int[] state) {
+		double res = 0;
+		for (int i = 0, j = state.length - 1; i < state.length; j = i++) {
+			double dx = x[state[i]] - x[state[j]];
+			double dy = y[state[i]] - y[state[j]];
+			res += Math.sqrt(dx * dx + dy * dy);
 		}
+		return res;
+	}
 
-//		try {
-//			PrintWriter pw = new PrintWriter("tsp.txt");
-//			pw.println(n);
-//			for (int i = 0; i < n; i++) {
-//				pw.println(x[i] + " " + y[i]);
-//			}
-//			pw.close();
-//		} catch (Exception e) {
-//		}
+	// visualization code
+	JPanel panel;
 
+	public SimulatedAnnealing() {
 		panel = new JPanel() {
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				Graphics2D g2 = ((Graphics2D) g);
-				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				g2.setStroke(new BasicStroke(3));
+				((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				((Graphics2D) g).setStroke(new BasicStroke(3));
 				g.setColor(Color.BLUE);
+				int maxx = getWidth() - 5;
+				int maxy = getHeight() - 30;
 				for (int i = 0, j = n - 1; i < n; j = i++) {
-					g.drawLine(x[p[i]], max - y[p[i]], x[p[j]], max - y[p[j]]);
+					g.drawLine((int) (x[p[i]] * maxx), (int) ((1 - y[p[i]]) * maxy), (int) (x[p[j]] * maxx),
+							(int) ((1 - y[p[j]]) * maxy));
 				}
 				g.setColor(Color.RED);
 				for (int i = 0; i < n; i++) {
-					g.drawOval(x[i], max - y[i], 3, 3);
+					g.drawOval((int) (x[i] * maxx) - 1, (int) ((1 - y[i]) * maxy) - 1, 3, 3);
 				}
 				g.setColor(Color.BLACK);
-				String s = String.format("%6.1f", eval(p));
-				g.drawString(s, 2, 410);
+				g.drawString(String.format("length = %6.1f", eval(p)), 2, maxy + 10);
 			}
 		};
 		setContentPane(panel);
