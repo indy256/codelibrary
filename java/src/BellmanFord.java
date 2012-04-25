800aacf14674d12c315d3ea3fd6240f50124e3ce
Andrey Naumenko <indy256@gmail.com>
2012-04-25 10:06:02 +0300

    update

--- java/src/BellmanFord.java
+++ java/src/BellmanFord.java
@@ -2,81 +2,90 @@ import java.util.*;
 
 public class BellmanFord {
 
-	static final int INF = Integer.MAX_VALUE / 3;
+	static final int INF = Integer.MAX_VALUE / 2;
 
-	static class Edge {
-		int u, v;
-		int cost;
+	public static class Edge {
+		int v, cost;
 
-		public Edge(int u, int v, int cost) {
-			this.u = u;
+		public Edge(int v, int cost) {
 			this.v = v;
 			this.cost = cost;
 		}
 	}
 
-	public static boolean bellmanFord(int n, List<Edge> edges, int s, int[] prio, int[] pred) {
+	public static boolean bellmanFord(List<Edge>[] graph, int s, int[] dist, int[] pred) {
 		Arrays.fill(pred, -1);
-		Arrays.fill(prio, INF);
-		prio[s] = 0;
-		boolean wasChanged = true;
-		for (int k = 0; k < n; k++) {
-			wasChanged = false;
-			for (Edge e : edges)
-				if (prio[e.v] > prio[e.u] + e.cost) {
-					prio[e.v] = prio[e.u] + e.cost;
-					pred[e.v] = e.u;
-					wasChanged = true;
+		Arrays.fill(dist, INF);
+		dist[s] = 0;
+		int n = graph.length;
+		boolean updated = false;
+		for (int step = 0; step < n; step++) {
+			updated = false;
+			for (int u = 0; u < n; u++) {
+				if (dist[u] < INF) {
+					for (Edge e : graph[u]) {
+						if (dist[e.v] > dist[u] + e.cost) {
+							dist[e.v] = Math.max(dist[u] + e.cost, -INF);
+							pred[e.v] = u;
+							updated = true;
+						}
+					}
 				}
-			if (!wasChanged)
+			}
+			if (!updated)
 				break;
 		}
-		// wasChanged is true iff graph has a negative cycle
-		return wasChanged;
+		// if updated is true then a negative cycle exists
+		return updated == false;
 	}
 
-	public static int[] findNegativeCycle(int n, List<Edge> edges) {
+	public static int[] findNegativeCycle(List<Edge>[] graph) {
+		int n = graph.length;
 		int[] pred = new int[n];
 		Arrays.fill(pred, -1);
-		int[] prio = new int[n];
-		Arrays.fill(prio, INF);
-		prio[0] = 0;
-		int last = 0;
-		for (int k = 0; k < n; k++) {
+		int[] dist = new int[n];
+		int last = -1;
+		for (int step = 0; step < n; step++) {
 			last = -1;
-			for (Edge e : edges)
-				if (prio[e.v] > prio[e.u] + e.cost) {
-					prio[e.v] = prio[e.u] + e.cost;
-					pred[e.v] = e.u;
-					last = e.v;
+			for (int u = 0; u < n; u++) {
+				for (Edge e : graph[u]) {
+					if (dist[e.v] > dist[u] + e.cost) {
+						dist[e.v] = Math.max(dist[u] + e.cost, -INF);
+						pred[e.v] = u;
+						last = e.v;
+					}
 				}
+			}
 			if (last == -1)
 				return null;
 		}
-		int[] path = new int[n];
-		int[] pos = new int[n];
-		for (int i = 0; ; i++) {
-			if (pos[last] != 0) {
-				int[] cycle = new int[i + 1 - pos[last]];
-				for (int j = 0; j < cycle.length; j++)
-					cycle[j] = path[i - 1 - j];
-				return cycle;
-			}
-			path[i] = last;
-			pos[last] = i + 1;
+		for (int i = 0; i < n; i++) {
 			last = pred[last];
 		}
+		int[] p = new int[n];
+		int cnt = 0;
+		for (int u = last; u != last || cnt == 0; u = pred[u]) {
+			p[cnt++] = u;
+		}
+		int[] cycle = new int[cnt];
+		for (int i = 0; i < cycle.length; i++) {
+			cycle[i] = p[--cnt];
+		}
+		return cycle;
 	}
 
 	// Usage example
 	public static void main(String[] args) {
-		List<Edge> edges = new ArrayList<Edge>();
-		edges.add(new Edge(0, 1, 1));
-		edges.add(new Edge(1, 0, 1));
-		edges.add(new Edge(1, 2, 1));
-		edges.add(new Edge(2, 3, -10));
-		edges.add(new Edge(3, 1, 1));
-		int[] cycle = findNegativeCycle(4, edges);
+		List<Edge>[] graph = new List[4];
+		for (int i = 0; i < graph.length; i++) {
+			graph[i] = new ArrayList<Edge>();
+		}
+		graph[0].add(new Edge(1, 1));
+		graph[1].add(new Edge(0, 1));
+		graph[1].add(new Edge(2, 1));
+		graph[2].add(new Edge(3, -10));
+		graph[3].add(new Edge(1, 1));
+		int[] cycle = findNegativeCycle(graph);
 		System.out.println(Arrays.toString(cycle));
 	}
 }
--- java/src/MinCostFlow.java
+++ java/src/MinCostFlow.java
@@ -2,58 +2,54 @@ import java.util.*;
 
 public class MinCostFlow {
 
-	// input: g - graph, s - source, t - sink
-	// output: maximum flow and minimum cost
-	public static long[] minCostFlow(Graph g, int s, int t) {
-		final long INF_D = 100000000000L;
-		long flow = 0;
-		long flowCost = 0;
-		long[] u = new long[g.n];
-		while (true) {
-			Queue<QItem> q = new PriorityQueue<QItem>();
-			q.add(new QItem(0, s));
-			Edge[] p = new Edge[g.n];
-			long[] prio = new long[g.n];
-			Arrays.fill(prio, INF_D);
-			prio[s] = 0;
-			while (!q.isEmpty()) {
-				QItem cur = q.poll();
-				if (cur.prio != prio[cur.v]) {
-					continue;
-				}
-				for (Edge e : g.edges[cur.v]) {
-					long nprio = prio[cur.v] + e.cost + u[cur.v] - u[e.t];
-					if (e.cap > e.f && prio[e.t] > nprio) {
-						prio[e.t] = nprio;
-						p[e.t] = e;
-						q.add(new QItem(nprio, e.t));
-					}
-				}
-			}
-			if (p[t] == null) {
-				break;
-			}
-			for (int i = 0; i < g.n; i++) {
-				if (p[i] != null) {
-					u[i] += prio[i];
-				}
-			}
-			Path path = getPath(p, s, t);
-			for (Edge e : path.edges) {
-				e.f += path.f;
-				g.edges[e.t].get(e.rev).f -= path.f;
-				flowCost += path.f * e.cost;
-			}
-			flow += path.f;
+	static final int INF = Integer.MAX_VALUE / 2;
+
+	List<Edge>[] graph;
+
+	void init(int n) {
+		graph = new List[n];
+		for (int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<Edge>();
 		}
-		return new long[] { flow, flowCost };
 	}
 
-	public static class QItem implements Comparable<QItem> {
-		long prio;
+	void addEdge(int s, int t, int cap, int cost) {
+		graph[s].add(new Edge(s, t, graph[t].size(), cap, cost));
+		graph[t].add(new Edge(t, s, graph[s].size() - 1, 0, -cost));
+	}
+
+	static class Edge {
+		public int u, v, rev, cap, f, cost;
+
+		public Edge(int u, int v, int rev, int cap, int cost) {
+			this.u = u;
+			this.v = v;
+			this.rev = rev;
+			this.cap = cap;
+			this.cost = cost;
+		}
+	}
+
+	static class Path {
+		public List<Edge> edges = new ArrayList<Edge>();
+		public int f;
+	}
+
+	static Path getPath(Edge[] p, int s, int t) {
+		Path path = new Path();
+		path.f = Integer.MAX_VALUE;
+		for (; t != s; t = p[t].u) {
+			path.edges.add(p[t]);
+			path.f = Math.min(path.f, p[t].cap - p[t].f);
+		}
+		return path;
+	}
+
+	static class QItem implements Comparable<QItem> {
+		int prio;
 		int v;
 
-		public QItem(long prio, int v) {
+		public QItem(int prio, int v) {
 			this.prio = prio;
 			this.v = v;
 		}
@@ -63,77 +59,96 @@ public class MinCostFlow {
 		}
 	}
 
-	public static class Graph {
-		public final int n;
-		public List<Edge>[] edges;
-
-		public Graph(int n) {
-			this.n = n;
-			edges = new List[n];
-			for (int i = 0; i < n; i++) {
-				edges[i] = new ArrayList<Edge>();
-			}
-		}
-
-		void addEdge(int s, int t, int cap, int cost) {
-			edges[s].add(new Edge(s, t, edges[t].size(), cap, cost));
-			edges[t].add(new Edge(t, s, edges[s].size() - 1, 0, -cost));
-		}
-
-		void clearFlow() {
-			for (List<Edge> edgesList : edges) {
-				for (Edge e : edgesList) {
-					e.f = 0;
+	static boolean bellmanFord(List<Edge>[] graph, int s, int[] dist) {
+		Arrays.fill(dist, INF);
+		dist[s] = 0;
+		int n = graph.length;
+		boolean updated = false;
+		for (int step = 0; step < n; step++) {
+			updated = false;
+			for (int u = 0; u < n; u++) {
+				if (dist[u] < INF) {
+					for (Edge e : graph[u]) {
+						if (e.cap > 0 && dist[e.v] > dist[u] + e.cost) {
+							dist[e.v] = Math.max(dist[u] + e.cost, -INF);
+							updated = true;
+						}
+					}
 				}
 			}
+			if (!updated)
+				break;
 		}
+		// if updated is true then a negative cycle exists
+		return updated == false;
 	}
 
-	public static class Edge {
-		public int s, t, rev, cap, f, cost;
-
-		public Edge(int s, int t, int rev, int cap, int cost) {
-			this.s = s;
-			this.t = t;
-			this.rev = rev;
-			this.cap = cap;
-			this.cost = cost;
+	public int[] minCostFlow(int s, int t) {
+		int[] pot = new int[graph.length];
+		if (!bellmanFord(graph, s, pot)) {
+			throw new IllegalArgumentException("Negative cycles are not supported");
 		}
-	}
-
-	public static class Path {
-		public List<Edge> edges = new ArrayList<Edge>();
-		public int f;
-	}
-
-	public static Path getPath(Edge[] p, int s, int t) {
-		Path path = new Path();
-		path.f = Integer.MAX_VALUE;
-		for (; t != s; t = p[t].s) {
-			path.edges.add(p[t]);
-			path.f = Math.min(path.f, p[t].cap - p[t].f);
+		int flow = 0;
+		int flowCost = 0;
+		while (true) {
+			Queue<QItem> q = new PriorityQueue<QItem>();
+			q.add(new QItem(0, s));
+			Edge[] p = new Edge[graph.length];
+			int[] prio = new int[graph.length];
+			Arrays.fill(prio, INF);
+			prio[s] = 0;
+			while (!q.isEmpty()) {
+				QItem cur = q.poll();
+				if (cur.prio != prio[cur.v]) {
+					continue;
+				}
+				for (Edge e : graph[cur.v]) {
+					int nprio = prio[cur.v] + e.cost + pot[cur.v] - pot[e.v];
+					if (e.cap > e.f && prio[e.v] > nprio) {
+						prio[e.v] = nprio;
+						p[e.v] = e;
+						q.add(new QItem(nprio, e.v));
+					}
+				}
+			}
+			if (p[t] == null) {
+				break;
+			}
+			for (int i = 0; i < graph.length; i++) {
+				if (p[i] != null) {
+					pot[i] += prio[i];
+				}
+			}
+			Path path = getPath(p, s, t);
+			for (Edge e : path.edges) {
+				e.f += path.f;
+				graph[e.v].get(e.rev).f -= path.f;
+				flowCost += path.f * e.cost;
+			}
+			flow += path.f;
 		}
-		return path;
+		return new int[] { flow, flowCost };
 	}
 
 	// Usage example
 	public static void main(String[] args) {
 		int[][] capacity = { { 0, 3, 2 }, { 0, 0, 2 }, { 0, 0, 0 } };
 		int n = capacity.length;
-		Graph g = new Graph(n);
+		MinCostFlow f = new MinCostFlow();
+		f.init(n);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (capacity[i][j] != 0) {
-					g.addEdge(i, j, capacity[i][j], 1);
+					f.addEdge(i, j, capacity[i][j], 1);
 				}
 			}
 		}
 		int s = 0;
 		int t = 2;
-		long[] res = minCostFlow(g, s, t);
-		long flow = res[0];
-		long flowCost = res[1];
-		System.out.println(flow == 4);
-		System.out.println(flowCost == 6);
+		int[] res = f.minCostFlow(s, t);
+		int flow = res[0];
+		int flowCost = res[1];
+		System.out.println(4 == flow);
+		System.out.println(6 == flowCost);
 	}
 }
