f57d3855b3b79308222b7476014a6364a3107f96
Andrey Naumenko <a_naumenko@adamantium.net>
2012-10-26 14:15:26 +0300

    update

--- java/src/HeavyLight.java
+++ java/src/HeavyLight.java
@@ -2,63 +2,130 @@ import java.util.*;
 
 public class HeavyLight {
 
-	int[] time_in;
-	int[] time_out;
-	int[] parent;
-	int[] subTreeSize;
-	int[] walk;
-	int time;
-	int cnt;
+	final int maxN = 100000;
+	int size[], p[];
+	List<Integer>[] c;
+	int tn, root[], num[];
+	int no[], pos[], t[];
+	int mpos, mem[];
+	int Time, t_in[], t_out[];
 
-	static void addEdge(List<Long>[] g, int a, int b, int cost) {
-		g[a].add(((long) b << 32) + cost);
-		g[b].add(((long) a << 32) + cost);
+	public HeavyLight() {
+		size = new int[maxN];
+		p = new int[maxN];
+		c = new List[maxN];
+		for (int i = 0; i < maxN; i++) {
+			c[i] = new ArrayList<Integer>();
+		}
+		root = new int[maxN];
+		num = new int[maxN];
+		no = new int[maxN];
+		pos = new int[maxN];
+		t = new int[maxN];
+		mem = new int[2 * maxN];
+		t_in = new int[maxN];
+		t_out = new int[maxN];
 	}
 
-	void dfs(List<Long>[] t, int u, int p, boolean[] vis) {
-		time_in[u] = time++;
-		vis[u] = true;
-		subTreeSize[u] = 1;
-		parent[u] = p;
-		for (long e : t[u]) {
-			int v = (int) (e >>> 32);
-			if (!vis[v]) {
-				dfs(t, v, u, vis);
-				subTreeSize[u] += subTreeSize[v];
+	void dfs(int v, int pr) {
+		int x;
+		t_in[v] = Time++;
+		p[v] = pr;
+		size[v] = 1;
+
+		for (int i = 0; i < c[v].size(); i++)
+			if ((x = c[v].get(i)) != pr) {
+				dfs(x, v);
+				size[v] += size[x];
+			}
+		t_out[v] = Time++;
+	}
+
+	int new_t(int v) {
+		root[tn] = v;
+		return tn++;
+	}
+
+	void build(int v, int v_no) {
+		int x;
+		no[v] = v_no;
+		pos[v] = num[v_no]++;
+		for (int i = 0; i < c[v].size(); i++)
+			if ((x = c[v].get(i)) != p[v])
+				build(x, 2 * size[x] > size[v] ? v_no : new_t(x));
+	}
+
+	void inc(int i, int j, int v) {
+		j += num[i];
+		// mem[t[i]][j] += v;
+		for (j /= 2; j > 0; j /= 2) {
+			// t[i][j] = Math.max(t[i][2 * (j)], t[i][2 * (j) + 1]);
+		}
+	}
+
+	int get(int i, int l, int r) {
+		int[] T = null;// t[i];
+		int N = num[i];
+		int res = 0;
+		for (l += N, r += N; l <= r; l /= 2, r /= 2) {
+			if (l % 2 == 1)
+				res = Math.max(res, T[l++]);
+			if (r % 2 == 0)
+				res = Math.max(res, T[r--]);
+		}
+		return res;
+	}
+
+	boolean ancestor(int i, int j) {
+		return t_in[i] <= t_in[j] && t_out[j] <= t_out[i];
+	}
+
+	public static void main(String[] args) {
+		HeavyLight hv = new HeavyLight();
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n - 1; i++) {
+			int a = sc.nextInt() - 1;
+			int b = sc.nextInt() - 1;
+
+			hv.c[a].add(b);
+			hv.c[b].add(a);
+		}
+
+		hv.dfs(0, -1);
+		hv.build(0, hv.new_t(0));
+
+		for (int i = 0; i < hv.tn; i++) {
+			// hv.t[i] = hv.mpos;
+			hv.mpos += 2 * hv.num[i];
+		}
+
+		int m = sc.nextInt();
+
+		while (m-- > 0) {
+			String c = sc.next();
+			if ("I".equals(c)) {
+				int a = sc.nextInt() - 1;
+				int b = sc.nextInt() - 1;
+				hv.inc(hv.no[a], hv.pos[a], b);
+			} else {
+				int a = sc.nextInt() - 1;
+				int b = sc.nextInt() - 1;
+				int res = 0;
+				for (int t = 0; t < 2; t++) {
+					int v;
+					while (!hv.ancestor(v = hv.root[hv.no[a]], b)) {
+						res = Math.max(res, hv.get(hv.no[a], 0, hv.pos[a]));
+						a = hv.p[v];
+					}
+					int x = a;
+					a = b;
+					b = x;
+				}
+				// printf("%d\n", max(res, get(no[a], min(pos[a], pos[b]), max(pos[a], pos[b]))));
 			}
 		}
-		walk[cnt++] = u;
-		time_out[u] = time++;
 	}
-
-	public void decompose(List<Long>[] t) {
-		int n = t.length;
-		time_in = new int[n];
-		time_out = new int[n];
-		parent = new int[n];
-		subTreeSize = new int[n];
-		walk = new int[n];
-		time = 0;
-		cnt = 0;
-
-		dfs(t, 0, -1, new boolean[n]);
-	}
-
-	// Usage example
-	public static void main(String[] args) {
-		List<Long>[] t = new List[8];
-		for (int i = 0; i < t.length; i++)
-			t[i] = new ArrayList<Long>();
-
-		addEdge(t, 0, 1, 0);
-		addEdge(t, 1, 3, 0);
-		addEdge(t, 1, 4, 0);
-		addEdge(t, 0, 2, 0);
-		addEdge(t, 2, 5, 1);
-		addEdge(t, 5, 6, 0);
-		addEdge(t, 5, 7, 0);
-
-		new HeavyLight().decompose(t);
-	}
-
 }
---
+++ java/src/Hungarian.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+public class Hungarian {
+
+	// a[1..n][1..m] >= 0, n <= m
+	public static int solveAssignmentProblem(int[][] a) {
+		int n = a.length - 1;
+		int m = a[0].length - 1;
+		int[] u = new int[n + 1];
+		int[] v = new int[m + 1];
+		int[] p = new int[m + 1];
+		int[] way = new int[m + 1];
+		for (int i = 1; i <= n; ++i) {
+			p[0] = i;
+			int j0 = 0;
+			int[] minv = new int[m + 1];
+			Arrays.fill(minv, Integer.MAX_VALUE);
+			boolean[] used = new boolean[m + 1];
+			do {
+				used[j0] = true;
+				int i0 = p[j0];
+				int delta = Integer.MAX_VALUE;
+				int j1 = 0;
+				for (int j = 1; j <= m; ++j)
+					if (!used[j]) {
+						int cur = a[i0][j] - u[i0] - v[j];
+						if (cur < minv[j]) {
+							minv[j] = cur;
+							way[j] = j0;
+						}
+						if (minv[j] < delta) {
+							delta = minv[j];
+							j1 = j;
+						}
+					}
+				for (int j = 0; j <= m; ++j)
+					if (used[j]) {
+						u[p[j]] += delta;
+						v[j] -= delta;
+					} else
+						minv[j] -= delta;
+				j0 = j1;
+			} while (p[j0] != 0);
+			do {
+				int j1 = way[j0];
+				p[j0] = p[j1];
+				j0 = j1;
+			} while (j0 != 0);
+		}
+		return -v[0];
+	}
+
+	// test code
+	static int solveAssignmentProblem2(int[][] a) {
+		int n = a.length - 1;
+		int m = a[0].length - 1;
+		int res = Integer.MAX_VALUE;
+		int[] p = new int[n];
+		for (int i = 0; i < n; i++)
+			p[i] = i;
+		do {
+			int cur = 0;
+			for (int i = 0; i < p.length; i++)
+				cur += a[i + 1][p[i] + 1];
+			res = Math.min(res, cur);
+		} while (nextArrangement(p, m));
+		return res;
+	}
+
+	static boolean nextArrangement(int[] p, int n) {
+		boolean[] used = new boolean[n];
+		for (int x : p) {
+			used[x] = true;
+		}
+		int m = p.length;
+		for (int i = m - 1; i >= 0; i--) {
+			used[p[i]] = false;
+			for (int j = p[i] + 1; j < n; j++) {
+				if (!used[j]) {
+					p[i++] = j;
+					used[j] = true;
+					for (int k = 0; k < n && i < m; k++) {
+						if (!used[k]) {
+							p[i++] = k;
+						}
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Random rnd = new Random(1);
+		for (int step = 0; step < 1000; step++) {
+			int n = rnd.nextInt(8) + 1;
+			int m = n + rnd.nextInt(9 - n);
+			int[][] a = new int[n + 1][m + 1];
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= m; j++) {
+					a[i][j] = rnd.nextInt(100000);
+				}
+			}
+			int res1 = solveAssignmentProblem(a);
+			int res2 = solveAssignmentProblem2(a);
+			if (res1 != res2) {
+				System.err.println(res1 + " " + res2);
+			}
+		}
+	}
+}
