995f90932afa29955aaa5e4e590af88e518329b4
Andrey Naumenko <a_naumenko@adamantium.net>
2012-11-02 18:10:22 +0300

    update

--- java/src/Trie.java
+++ java/src/Trie.java
@@ -6,34 +6,32 @@ public class Trie {
 		boolean leaf;
 	}
 
-	TrieNode root = new TrieNode();
-
-	void insertString(String s) {
+	public static void insertString(TrieNode root, String s) {
 		TrieNode v = root;
 		for (char ch : s.toCharArray()) {
-			if (!v.children.containsKey(ch)) {
-				v.children.put(ch, new TrieNode());
-			}
-			v = v.children.get(ch);
+			TrieNode next = v.children.get(ch);
+			if (next == null)
+				v.children.put(ch, next = new TrieNode());
+			v = next;
 		}
 		v.leaf = true;
 	}
 
-	// Usage example
-	public static void main(String[] args) {
-		Trie trie = new Trie();
-		trie.insertString("hello");
-		trie.insertString("world");
-		trie.insertString("hi");
-		printSorted(trie.root, "");
-	}
-
-	static void printSorted(TrieNode node, String s) {
+	public static void printSorted(TrieNode node, String s) {
 		for (Character ch : node.children.keySet()) {
 			printSorted(node.children.get(ch), s + ch);
 		}
 		if (node.leaf) {
 			System.out.println(s);
 		}
+	}
+
+	// Usage example
+	public static void main(String[] args) {
+		TrieNode root = new TrieNode();
+		insertString(root, "hello");
+		insertString(root, "world");
+		insertString(root, "hi");
+		printSorted(root, "");
 	}
 }
