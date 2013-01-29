c9c9132fe07e8a81eabc774071d9759e3b2bc05a
indy256 <indy256@gmail.com>
2013-01-30 02:48:26 +0300

    update

--- java/src/ContractionHierarchies.java
+++ java/src/ContractionHierarchies.java
@@ -9,24 +9,19 @@ public class ContractionHierarchies {
 	final int EDGES = 1000;
 
 	int[] levels = new int[NODES];
-	int[] tail = new int[NODES];
-	int[] rtail = new int[NODES];
 
 	int[] len = new int[EDGES];
 	int[] u = new int[EDGES];
 	int[] v = new int[EDGES];
-	int[] prev = new int[EDGES];
 
-	int[] rlen = new int[EDGES];
-	int[] ru = new int[EDGES];
-	int[] rv = new int[EDGES];
-	int[] rprev = new int[EDGES];
+	int[][] tail = {new int[NODES], new int[NODES]};
+	int[][] prev = {new int[EDGES], new int[EDGES]};
 
 	{
-		Arrays.fill(prev, -1);
-		Arrays.fill(rprev, -1);
-		Arrays.fill(tail, -1);
-		Arrays.fill(rtail, -1);
+		Arrays.fill(prev[0], -1);
+		Arrays.fill(prev[1], -1);
+		Arrays.fill(tail[0], -1);
+		Arrays.fill(tail[1], -1);
 		for (int i = 0; i < levels.length; i++)
 			levels[i] = i;
 	}
@@ -34,39 +29,37 @@ public class ContractionHierarchies {
 	int edges = 0;
 	int nodes = 0;
 
-	public void addEdge(int u, int v, int len) {
-		nodes = Math.max(nodes, u + 1);
-		nodes = Math.max(nodes, v + 1);
+	public void addEdge(int s, int t, int len) {
+		nodes = Math.max(nodes, s + 1);
+		nodes = Math.max(nodes, t + 1);
 
-		// add (u,v) to normal graph
-		this.u[edges] = u;
-		this.v[edges] = v;
 		this.len[edges] = len;
-		prev[edges] = tail[u];
-		tail[u] = edges;
+		u[edges] = s;
+		v[edges] = t;
 
-		// add (v,u) to reverse graph
-		ru[edges] = v;
-		rv[edges] = u;
-		rlen[edges] = len;
-		rprev[edges] = rtail[v];
-		rtail[v] = edges;
+		// add (s,t) to normal graph
+		prev[0][edges] = tail[0][s];
+		tail[0][s] = edges;
+
+		// add (t,s) to reverse graph
+		prev[1][edges] = tail[1][t];
+		tail[1][t] = edges;
 
 		++edges;
 	}
 
 	public void preprocess() {
 		for (int v = 0; v < nodes - 2; v++) {
-			for (int vw = tail[v]; vw != -1; vw = prev[vw]) {
+			for (int vw = tail[0][v]; vw != -1; vw = prev[0][vw]) {
 				int w = this.v[vw];
 				if (levels[w] <= levels[v])
 					continue;
-				for (int uv = rtail[v]; uv != -1; uv = rprev[uv]) {
-					int u = rv[uv];
+				for (int uv = tail[1][v]; uv != -1; uv = prev[1][uv]) {
+					int u = this.u[uv];
 					if (levels[u] <= levels[v] || u == w)
 						continue;
 
-					addEdge(u, v, rlen[uv] + len[vw]);
+					addEdge(u, v, len[uv] + len[vw]);
 					System.out.println("(" + u + "," + v + ")");
 				}
 			}
@@ -74,27 +67,37 @@ public class ContractionHierarchies {
 	}
 
 	public int shortestPath(int s, int t) {
-		int[] prio1 = new int[nodes];
-		int[] prio2 = new int[nodes];
-		Arrays.fill(prio1, Integer.MAX_VALUE/2);
-		Arrays.fill(prio2, Integer.MAX_VALUE/2);
-		prio1[s] = 0;
-		prio2[t] = 0;
-		PriorityQueue<Long> q1 = new PriorityQueue<Long>();
-		q1.add((long) s);
-		PriorityQueue<Long> q2 = new PriorityQueue<Long>();
-		q2.add((long) t);
+		int[][] prio = {new int[nodes], new int[nodes]};
+		Arrays.fill(prio[0], Integer.MAX_VALUE / 2);
+		Arrays.fill(prio[1], Integer.MAX_VALUE / 2);
+		prio[0][s] = 0;
+		prio[1][t] = 0;
+		PriorityQueue<Long>[] q = new PriorityQueue[]{new PriorityQueue<Long>(), new PriorityQueue<Long>()};
+		q[0].add((long) s);
+		q[1].add((long) t);
 		int res = Integer.MAX_VALUE;
-		while (!q1.isEmpty() || !q2.isEmpty()) {
-			PriorityQueue<Long> q = q2.isEmpty() || !q1.isEmpty() && q1.peek() < q2.peek() ? q1 : q2;
-			long cur = q.poll();
+		for (int dir = 0; !q[0].isEmpty() || !q[1].isEmpty(); ) {
+			long min1 = q[0].isEmpty() ? Long.MAX_VALUE : q[0].peek();
+			long min2 = q[1].isEmpty() ? Long.MAX_VALUE : q[1].peek();
+			long min = Math.min(min1, min2);
+			if (res < min)
+				break;
+			long cur = q[dir].poll();
 			if (res < cur >>> 32)
 				break;
-			int curu = (int) cur;
-			res = Math.min(res, prio1[curu] + prio2[curu]);
+			int u = (int) cur;
+			res = Math.min(res, prio[dir][u] + prio[1 - dir][u]);
 
-
-
+			for (int edge = tail[dir][u]; edge != -1; edge = prev[dir][edge]) {
+				int v = this.v[edge];
+				int nprio = prio[dir][u] + len[edge];
+				if (prio[dir][v] > nprio) {
+					prio[dir][v] = nprio;
+					q[dir].add(((long) nprio << 32) + v);
+				}
+			}
+			if (!q[1 - dir].isEmpty())
+				dir = 1 - dir;
 		}
 
 		return res;
