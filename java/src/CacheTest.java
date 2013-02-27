dc3694323079fc735920c98f3bde58e27320feba
Andrey Naumenko <a_naumenko@adamantium.net>
2013-02-27 14:34:06 +0300

    update

--- java/src/CacheTest.java
+++ java/src/CacheTest.java
@@ -6,135 +6,165 @@ import java.util.*;
 
 public class CacheTest {
 
-	public static class Cache {
-		final int[] ids;
-		final Object[] o;
+    public static class ArrayCache {
+        final Object[] objects;
 
-		public Cache(int[] ids, Object[] o) {
-			this.ids = ids.clone();
-			Arrays.sort(ids);
-			this.o = o;
-		}
+        public ArrayCache(int[] ids, Object[] objects) {
+            this.objects = new Object[ids[ids.length - 1] + 1];
+            for (int i = 0; i < ids.length; i++) {
+                this.objects[ids[i]] = objects[i];
+            }
+        }
 
-		public Object get(int id) {
-//			if(true)
-//				return o[id];
-			int pos = Arrays.binarySearch(ids, id);
-			return pos >= 0 ? o[pos] : null;
-		}
-	}
+        public Object get(int id) {
+            return id < objects.length ? objects[id] : null;
+        }
+    }
 
-	public static class Cache2 {
-		final int[] cnt;
-		final int[] a;
-		final Object[] o;
-		final int mask;
+    public static class BinarySearchCache {
+        final int[] ids;
+        final Object[] objects;
 
-		public Cache2(int[] ids, Object[] o) {
-			int n = 1;
-			while (n < ids.length)
-				n *= 2;
-			mask = n - 1;
-			cnt = new int[n + 1];
-			for (int id : ids)
-				++cnt[id & mask];
-			for (int i = 1; i < cnt.length; i++)
-				cnt[i] += cnt[i - 1];
-			a = new int[ids.length];
-			this.o = new Object[o.length];
-			for (int i = ids.length-1; i >=0; i--) {
-				int id = ids[i];
-				a[--cnt[id & mask]] = id;
-				this.o[cnt[id & mask]] = o[i];
-			}
-		}
+        public BinarySearchCache(int[] ids, Object[] objects) {
+            this.ids = ids.clone();
+            this.objects = objects;
+        }
 
-		public Object get(int id) {
-			int bucket = id & mask;
-			int pos = Arrays.binarySearch(a, cnt[bucket], cnt[bucket + 1], id);
-			return pos >= 0 ? o[pos] : null;
-		}
-	}
+        public Object get(int id) {
+            int pos = Arrays.binarySearch(ids, id);
+            return pos >= 0 ? objects[pos] : null;
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(new File("E:/Dropbox/map/b2.txt"));
-		List<Integer> list = new ArrayList<>();
-		int cnt = 0;
-		while (sc.hasNext()) {
-//			list.add(sc.nextInt());
-			sc.nextInt();
-			list.add(cnt++);
-		}
-		list.clear();
-		int n = 1000000;
-		Random rnd = new Random(1);
-		cnt = 0;
-		for (int i = 0; i < 500000; i++) {
-			//list.add(rnd.nextInt(n));
-			list.add(cnt++);
-			if (rnd.nextInt(10) == 5) ++cnt;
-		}
+    public static class SortedBucketsCache {
+        final int[] cnt;
+        final int[] a;
+        final Object[] objects;
+        final int mask;
 
-		Collections.sort(list);
-		int[] ids = new int[list.size()];
-		Object[] o = new Object[n];
-		Map<Integer, Object> map = new HashMap<>();
-		Int2ObjectMap map2 = new Int2ObjectOpenHashMap();
-		for (int i = 0; i < ids.length; i++) {
-			ids[i] = list.get(i);
-			o[ids[i]] = list.get(i);
-			map.put(ids[i], o[ids[i]]);
-			map2.put(ids[i], o[ids[i]]);
-		}
+        public SortedBucketsCache(int[] ids, Object[] obj) {
+            int n = 1;
+            while (n < ids.length)
+                n *= 2;
+            mask = n - 1;
+            cnt = new int[n + 1];
+            for (int id : ids)
+                ++cnt[id & mask];
+            for (int i = 1; i < cnt.length; i++)
+                cnt[i] += cnt[i - 1];
+            a = new int[ids.length];
+            objects = new Object[obj.length];
+            for (int i = ids.length - 1; i >= 0; i--) {
+                int id = ids[i];
+                a[--cnt[id & mask]] = id;
+                objects[cnt[id & mask]] = obj[i];
+            }
+        }
 
-		Cache cache = new Cache(ids, o);
-		Object[] o1 = new Object[n];
-		long time = System.currentTimeMillis();
-		int cnt1 = 0;
-		int steps = 100;
-		for (int step = 0; step < steps; step++) {
-			for (int i = 0; i < ids.length; i++) {
-//				cache.get(ids[i]);
-				Object a= o[ids[i]];
-				++cnt1;
-			}
-		}
-		System.out.println(System.currentTimeMillis() - time);
+        public Object get(int id) {
+            int bucket = id & mask;
+            int pos = Arrays.binarySearch(a, cnt[bucket], cnt[bucket + 1], id);
+            return pos >= 0 ? objects[pos] : null;
+        }
+    }
 
-		Cache2 cache2 = new Cache2(ids, o);
-		time = System.currentTimeMillis();
-		int cnt4 = 0;
-		for (int step = 0; step < steps; step++) {
-			for (int i = 0; i < ids.length; i++) {
-				cache2.get(ids[i]);
-				++cnt4;
-			}
-		}
-		System.out.println(System.currentTimeMillis() - time);
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("D:/Dropbox/map/b.txt"));
+        List<Integer> list = new ArrayList<>();
+        while (sc.hasNext()) {
+            list.add(sc.nextInt());
+        }
+        for (int i = 0; i + 1 < list.size(); i++) if (list.get(i) >= list.get(i + 1)) throw new RuntimeException();
+        final int steps = 1000;
+//        list.clear();
+//        Random rnd = new Random(1);
+//        for (int i = 0; i < 200000; i++) {
+//            list.add(rnd.nextInt(n));
+//        }
 
-		time = System.currentTimeMillis();
-		int cnt2 = 0;
-		for (int step = 0; step < steps; step++) {
-			for (int i = 0; i < ids.length; i++) {
-//				map.put(i,o[i]);
-				Object a = map.get(ids[i]);
-				++cnt2;
-			}
-		}
+        Collections.sort(list);
+        List<Integer> list2 = new ArrayList<>(list);
+        Collections.shuffle(list2);
+        int[] access = new int[list2.size()];
+        for (int i = 0; i < access.length; i++) {
+            access[i] = list2.get(i);
+        }
+        int[] ids = new int[list.size()];
+        Object[] objects = new Object[ids.length];
+        for (int i = 0; i < ids.length; i++) {
+            ids[i] = list.get(i);
+            objects[i] = list.get(i);
+        }
 
-		System.out.println(System.currentTimeMillis() - time);
+        Map<Integer, Object> map1 = new HashMap<>();
+        Int2ObjectMap map2 = new Int2ObjectOpenHashMap();
+        ArrayCache arrayCache = new ArrayCache(ids, objects);
+        BinarySearchCache binarySearchCache = new BinarySearchCache(ids, objects);
+        SortedBucketsCache sortedBucketsCache = new SortedBucketsCache(ids, objects);
+        for (int i = 0; i < ids.length; i++) {
+            map1.put(ids[i], objects[i]);
+            map2.put(ids[i], objects[i]);
+        }
 
-		time = System.currentTimeMillis();
-		int cnt3 = 0;
-		for (int step = 0; step < steps; step++) {
-			for (int i = 0; i < ids.length; i++) {
-//				map2.put(i,o[i]);
-				Object a = map2.get(ids[i]);
-				++cnt3;
-			}
-		}
+        for (int i = 0; i < 1000000; i++) {
+            Object o1 = arrayCache.get(i);
+            Object o2 = binarySearchCache.get(i);
+            Object o3 = sortedBucketsCache.get(i);
+            Object o4 = map1.get(i);
+            Object o5 = map2.get(i);
+            if (o1 != o2 || o1 != o3 || o1 != o4 || o1 != o5)
+                throw new RuntimeException();
+        }
 
-		System.out.println(System.currentTimeMillis() - time);
-		System.out.println(cnt1 + cnt2 + cnt3 + cnt4);
-	}
+        long time = System.currentTimeMillis();
+        Object[] objects2 = new Object[200000];
+        for (int i = 0; i < objects2.length; i++) objects2[i] = i;
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = objects2[access[i]];
+//                int o = access[i];
+            }
+        }
+        System.out.println("array = " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = arrayCache.get(access[i]);
+            }
+        }
+        System.out.println("arrayCache = " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = sortedBucketsCache.get(access[i]);
+            }
+        }
+        System.out.println("sortedBucketsCache = " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = map2.get(access[i]);
+            }
+        }
+        System.out.println("Int2ObjectOpenHashMap = " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = binarySearchCache.get(access[i]);
+            }
+        }
+        System.out.println("binarySearchCache = " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int step = 0; step < steps; step++) {
+            for (int i = 0; i < ids.length; i++) {
+                Object o = map1.get(access[i]);
+            }
+        }
+        System.out.println("HashMap = " + (System.currentTimeMillis() - time));
+    }
 }
