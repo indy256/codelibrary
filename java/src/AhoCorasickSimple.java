34f4eafb1594ac92cb05805983aa093508f34157
andrey naumenko <a_naumenko@adamantium.net>
2013-05-26 01:21:27 +0300

    update

--- java/src/AhoCorasickSimple.java
+++ java/src/AhoCorasickSimple.java
@@ -3,32 +3,32 @@ import java.util.*;
 public class AhoCorasickSimple {
 
 	static final int ALPHABET_SIZE = 26;
-	static final int TOTAL_SYMBOLS = 1000;
 
-	int[][] graph = new int[TOTAL_SYMBOLS + 1][ALPHABET_SIZE];
-	String[] prefixes = new String[TOTAL_SYMBOLS + 1];
+	String[] prefixes;
 
-	int go(String s, char ch) {
-		s += ch;
-		int best = -1;
-		int res = -1;
-		for (int i = 0; i < prefixes.length; i++)
-			if (best < prefixes[i].length() && s.endsWith(prefixes[i])) {
-				best = prefixes[i].length();
-				res = i;
+	public int[][] buildAutomata(String[] words) {
+		Map<String, Integer> prefixMap = new TreeMap<String, Integer>();
+		for (String s : words) {
+			for (int i = 0; i <= s.length(); i++) {
+				prefixMap.put(s.substring(0, i), 0);
 			}
-		return res;
-	}
-
-	public void init(String[] words) {
-		Set<String> prefixSet = new TreeSet<String>();
-		for (String s : words)
-			for (int i = 0; i <= s.length(); i++)
-				prefixSet.add(s.substring(0, i));
-		prefixes = prefixSet.toArray(new String[0]);
-		for (int i = 0; i < prefixes.length; i++)
-			for (int j = 0; j < ALPHABET_SIZE; j++)
-				graph[i][j] = go(prefixes[i], (char) ('a' + j));
+		}
+		prefixes = prefixMap.keySet().toArray(new String[0]);
+		for (int i = 0; i < prefixes.length; i++) {
+			prefixMap.put(prefixes[i], i);
+		}
+		int[][] graph = new int[prefixes.length][ALPHABET_SIZE];
+		for (int i = 0; i < prefixes.length; i++) {
+			prefixMap.put(prefixes[i], i);
+			for (int j = 0; j < ALPHABET_SIZE; j++) {
+				String s = prefixes[i] + (char) ('a' + j);
+				while (!prefixMap.containsKey(s)) {
+					s = s.substring(1);
+				}
+				graph[i][j] = prefixMap.get(s);
+			}
+		}
+		return graph;
 	}
 
 	// Usage example
