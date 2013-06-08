ca05e064aeb4426159bb213f534ea6bcb8103793
andrey naumenko <a_naumenko@adamantium.net>
2013-06-08 22:59:19 +0300

    update

--- java/src/FenwickTreeExtended.java
+++ java/src/FenwickTreeExtended.java
@@ -1,5 +1,6 @@
 public class FenwickTreeExtended {
 
+	// T[i] += value
 	public static void add(int[] t, int i, int value) {
 		for (; i < t.length; i += (i + 1) & -(i + 1))
 			t[i] += value;
@@ -44,16 +45,33 @@ public class FenwickTreeExtended {
 		add(t, i, -get(t, i) + value);
 	}
 
-	// interval add trick
-	public void rev_add(int[] t, int a, int b, int value) {
+	///////////////////////////////////////////////////////
+	// interval add
+	public static void add(int[] t, int a, int b, int value) {
 		add(t, a, value);
 		add(t, b + 1, -value);
 	}
 
-	// get for interval add trick
-	public int rev_get(int[] t, int i) {
+	// point query
+	public static int get1(int[] t, int i) {
 		return sum(t, i);
 	}
+	///////////////////////////////////////////////////////
+
+	///////////////////////////////////////////////////////
+	// interval add
+	public static void add(int[] t1, int[] t2, int a, int b, int value) {
+		add(t1, a, value);
+		add(t1, b, -value);
+		add(t2, a, -value * (a - 1));
+		add(t2, b, value * b);
+	}
+
+	// interval query
+	public static int sum(int[] t1, int[] t2, int i) {
+		return sum(t1, i) * i + sum(t2, i);
+	}
+	///////////////////////////////////////////////////////
 
 	// Returns min(p|sum[0,p]>=sum)
 	public static int lower_bound(int[] t, int sum) {
@@ -82,5 +100,11 @@ public class FenwickTreeExtended {
 		System.out.println();
 		t = createTreeFromArray(new int[]{0, 0, 1, 0, 0, 1, 0, 0});
 		System.out.println(5 == lower_bound(t, 2));
+
+		int[] t1 = new int[10];
+		int[] t2 = new int[10];
+		add(t1, t2, 0, 9, 1);
+		add(t1, t2, 0, 0, -2);
+		System.out.println(sum(t1, t2, 9));
 	}
 }
