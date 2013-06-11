0ea411e417ee66767caa239564269225c9314bd0
Andrey Naumenko <a_naumenko@adamantium.net>
2013-06-11 12:29:42 +0300

    update

--- java/src/BinarySearch.java
+++ java/src/BinarySearch.java
@@ -2,7 +2,8 @@ import java.util.function.*;
 
 public class BinarySearch {
 
-	public static int integerBinarySearchFirstTrue(IntPredicate predicate, int from, int to) {
+	// 000[1]11
+	public static int binarySearchFirstTrue(IntPredicate predicate, int from, int to) {
 		int lo = from - 1;
 		int hi = to + 1;
 		while (hi - lo > 1) {
@@ -16,7 +17,7 @@ public class BinarySearch {
 		return hi;
 	}
 
-	public static double doubleBinarySearchFirstTrue(DoublePredicate predicate, double lo, double hi) {
+	public static double binarySearch(DoublePredicate predicate, double lo, double hi) {
 		for (int step = 0; step < 1000; step++) {
 			double mid = (lo + hi) / 2;
 			if (!predicate.test(mid)) {
@@ -31,7 +32,7 @@ public class BinarySearch {
 	// Usage example
 	public static void main(String[] args) {
 		int[] a = {1, 2, 5, 6, 7};
-		System.out.println(3 == integerBinarySearchFirstTrue(i -> a[i] >= 6, 0, a.length - 1));
-		System.out.println(Math.sqrt(2) == doubleBinarySearchFirstTrue(x -> x * x >= 2, 0, 2));
+		System.out.println(3 == binarySearchFirstTrue(i -> a[i] >= 6, 0, a.length - 1));
+		System.out.println(Math.sqrt(2) == binarySearch(x -> x * x >= 2, 0, 2));
 	}
 }
