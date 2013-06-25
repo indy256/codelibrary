370d8c81d4b97f949744358805a346af919408dc
andrey naumenko <a_naumenko@adamantium.net>
2013-06-26 02:11:35 +0300

    update

---
+++ java/src/ConvexHullOptimization.java
@@ -0,0 +1,34 @@
+public class ConvexHullOptimization {
+
+	long[] A = new long[1000000];
+	long[] B = new long[1000000];
+	int len;
+	int ptr;
+
+	// a descends
+	public void addLine(long a, long b) {
+		len += 2;
+		do {
+			--len;
+			A[len - 1] = a;
+			B[len - 1] = b;
+		} while (len >= 3 && (B[len - 1] - B[len - 2]) * (A[len - 3] - A[len - 2]) < (B[len - 2] - B[len - 3]) * (A[len - 2] - A[len - 1]));
+	}
+
+	// x ascends
+	public long minValue(int x) {
+		while (ptr + 1 < len && A[ptr] * x + B[ptr] > A[ptr + 1] * x + B[ptr + 1]) {
+			++ptr;
+		}
+		return A[ptr] * x + B[ptr];
+	}
+
+	public static void main(String[] args) {
+		ConvexHullOptimization h = new ConvexHullOptimization();
+		h.addLine(10, 1);
+		h.addLine(5, 2);
+		h.addLine(3, 1);
+		h.addLine(1, 1);
+		System.out.println(h.minValue(3));
+	}
+}
