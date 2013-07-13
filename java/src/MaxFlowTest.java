9493eae9c2492cc9e477b2411f61b97efbcc0b15
andrey naumenko <a_naumenko@adamantium.net>
2013-07-13 18:41:13 +0300

    update

--- java/src/MaxFlowTest.java
+++ java/src/MaxFlowTest.java
@@ -1,26 +1,23 @@
+import obsolete.MaxFlowDinicMatrix;
+
 import java.util.List;
 import java.util.Random;
 
-import obsolete.MaxFlowDinicMatrix;
-
 public class MaxFlowTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int V = 1500;
 		int E = 1000000;
 		Random rnd = new Random(1);
 		List<Integer>[] g = RandomGraph.getRandomUndirectedConnectedGraph(V, E, rnd);
-//		MaxFlowDinic f1 = new MaxFlowDinic();
+		MaxFlowDinic f1 = new MaxFlowDinic();
 		MaxFlowDinicMatrix f2 = new MaxFlowDinicMatrix();
 		MaxFlowPreflowN3 f3 = new MaxFlowPreflowN3();
 		MaxFlowRetreat f4 = new MaxFlowRetreat();
 
 		System.out.println("starting");
 
-//		f1.init(V);
+		final List<MaxFlowDinic.Edge>[] g1 = MaxFlowDinic.createGraph(V);
 		f2.init(V);
 		f3.init(V);
 		f4.init(V);
@@ -28,7 +25,7 @@ public class MaxFlowTest {
 		for (int i = 0; i < g.length; i++) {
 			for (int j : g[i]) {
 				int cap = rnd.nextInt(100) + 1;
-//				f1.addEdge(i, j, cap);
+				MaxFlowDinic.addEdge(g1, i, j, cap);
 				f2.addEdge(i, j, cap);
 				f3.addEdge(i, j, cap);
 				f4.addEdge(i, j, cap);
@@ -36,8 +33,8 @@ public class MaxFlowTest {
 		}
 
 		long time = System.currentTimeMillis();
-//		int flow1 = f1.maxFlow(0, V - 1);
-//		System.out.println(System.currentTimeMillis() - time);
+		int flow1 = MaxFlowDinic.maxFlow(g1, 0, V - 1);
+		System.out.println(System.currentTimeMillis() - time);
 
 		time = System.currentTimeMillis();
 		int flow2 = f2.maxFlow(0, V - 1);
@@ -51,6 +48,6 @@ public class MaxFlowTest {
 		int flow4 = f4.maxFlow(0, V - 1);
 		System.out.println(System.currentTimeMillis() - time);
 
-		System.out.println(/*flow1 + */" " + flow2 + " " + flow3 + " " + flow4);
+		System.out.println(flow1 + " " + flow2 + " " + flow3 + " " + flow4);
 	}
 }
