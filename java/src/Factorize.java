401c51f3a788e0ea6c00015493a03e2684116ede
Andrey Naumenko <indy256@gmail.com>
2013-12-07 07:05:24 +0300

    update

--- java/src/Factorize.java
+++ java/src/Factorize.java
@@ -3,20 +3,20 @@ import java.util.*;
 public class Factorize {
 
     // returns map: prime_divisor -> power
-	public static TreeMap<Long, Integer> factorize(long n) {
-		TreeMap<Long, Integer> factors = new TreeMap<>();
-		for (long divisor = 2; n > 1; ) {
+	public static Map<Long, Integer> factorize(long n) {
+		Map<Long, Integer> factors = new LinkedHashMap<>();
+		for (long d = 2; n > 1; ) {
 			int power = 0;
-			while (n % divisor == 0) {
+			while (n % d == 0) {
 				++power;
-				n /= divisor;
+				n /= d;
 			}
 			if (power > 0) {
-				factors.put(divisor, power);
+				factors.put(d, power);
 			}
-			++divisor;
-			if (divisor * divisor > n) {
-				divisor = n;
+			++d;
+			if (d * d > n) {
+				d = n;
 			}
 		}
 		return factors;
@@ -39,7 +39,7 @@ public class Factorize {
 
 	// Usage example
 	public static void main(String[] args) {
-		TreeMap<Long, Integer> f = factorize(24);
+		Map<Long, Integer> f = factorize(24);
 		System.out.println(f);
 		System.out.println(Arrays.toString(getAllDivisors(16)));
 	}
