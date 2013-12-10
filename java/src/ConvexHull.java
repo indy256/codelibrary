b3534a178549f627aa08f630330198e4f44a784e
Andrey Naumenko <indy256@gmail.com>
2013-12-10 05:15:46 +0300

    update

--- java/src/ConvexHull.java
+++ java/src/ConvexHull.java
@@ -1,6 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class ConvexHull {
+
+	public static Point[] convexHull(Point[] p) {
+		int n = p.length;
+		if (n <= 1)
+			return p;
+		Arrays.sort(p);
+		Point[] q = new Point[n * 2];
+		int cnt = 0;
+		for (int i = 0; i < n; q[cnt++] = p[i++])
+			for (; cnt > 1 && cross(q[cnt - 2], q[cnt - 1], p[i]) >= 0; --cnt)
+				;
+		for (int i = n - 2, t = cnt; i >= 0; q[cnt++] = p[i--])
+			for (; cnt > t && cross(q[cnt - 2], q[cnt - 1], p[i]) >= 0; --cnt)
+				;
+		return Arrays.copyOf(q, cnt - 1 - (q[0].compareTo(q[1]) == 0 ? 1 : 0));
+	}
+
+	static long cross(Point a, Point b, Point c) {
+		return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+	}
 
 	static class Point implements Comparable<Point> {
 		long x, y;
@@ -15,26 +35,6 @@ public class ConvexHull {
 		}
 	}
 
-	static boolean cw(Point a, Point b, Point c) {
-		return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x) < 0;
-	}
-
-	public static Point[] convexHull(Point[] p) {
-		int n = p.length;
-		if (n <= 1)
-			return p;
-		Arrays.sort(p);
-		Point[] q = new Point[n * 2];
-		int cnt = 0;
-		for (int i = 0; i < n; q[cnt++] = p[i++])
-			for (; cnt > 1 && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
-				;
-		for (int i = n - 2, t = cnt; i >= 0; q[cnt++] = p[i--])
-			for (; cnt > t && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
-				;
-		return Arrays.copyOf(q, cnt - 1 - (q[0].compareTo(q[1]) == 0 ? 1 : 0));
-	}
-
 	// Usage example
 	public static void main(String[] args) {
 		Point[] points = new Point[4];
@@ -44,5 +44,12 @@ public class ConvexHull {
 		points[3] = new Point(1, 1);
 		Point[] hull = convexHull(points);
 		System.out.println(3 == hull.length);
+
+		Random rnd = new Random(1);
+		int n = 1_000_000;
+		points = new Point[n];
+		for (int i = 0; i < n; i++)
+			points[i] = new Point(rnd.nextInt(10_000), rnd.nextInt(10_000));
+		System.out.println(convexHull(points).length);
 	}
 }
