83208769672175989632e2210e20d4a72f8fc2ff
Andrey Naumenko <indy256@gmail.com>
2013-12-19 10:14:41 +0300

    update

--- java/src/BinaryHeap.java
+++ java/src/BinaryHeap.java
@@ -7,41 +7,25 @@ public class BinaryHeap {
 	}
 
 	// build heap in O(n)
-	public BinaryHeap(int[] keys) {
-		heap = keys.clone();
-		size = keys.length;
+	public BinaryHeap(int[] values) {
+		heap = values.clone();
+		size = values.length;
 		for (int pos = size / 2 - 1; pos >= 0; pos--) {
-			moveDown(pos);
+			pushDown(pos);
 		}
-	}
-
-	public void add(int node) {
-		heap[size++] = node;
-		moveUp(heap[size - 1]);
 	}
 
 	public int removeMin() {
 		int removed = heap[0];
 		heap[0] = heap[--size];
-		moveDown(0);
+		pushDown(0);
 		return removed;
 	}
 
-	void moveUp(int pos) {
-		while (pos > 0) {
-			int parent = (pos - 1) / 2;
-			if (heap[pos] >= heap[parent]) {
-				break;
-			}
-			swap(heap, pos, parent);
-			pos = parent;
-		}
-	}
-
-	void moveDown(int pos) {
-		while (pos < size / 2) {
+	void pushDown(int pos) {
+		while (2 * pos + 1 < size) {
 			int child = 2 * pos + 1;
-			if (child + 1 < size && heap[child] > heap[child + 1]) {
+			if (child + 1 < size && heap[child + 1] < heap[child]) {
 				++child;
 			}
 			if (heap[pos] <= heap[child]) {
@@ -49,6 +33,22 @@ public class BinaryHeap {
 			}
 			swap(heap, pos, child);
 			pos = child;
+		}
+	}
+
+	public void add(int node) {
+		heap[size++] = node;
+		popUp(heap[size - 1]);
+	}
+
+	void popUp(int pos) {
+		while (pos > 0) {
+			int parent = (pos - 1) / 2;
+			if (heap[pos] >= heap[parent]) {
+				break;
+			}
+			swap(heap, pos, parent);
+			pos = parent;
 		}
 	}
 
