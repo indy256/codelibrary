8680bf29c38ab9d8d63000b4b64ead0a5393f75a
Andrey Naumenko <indy256@gmail.com>
2014-01-05 00:07:31 +0300

    update

--- java/src/Permutations.java
+++ java/src/Permutations.java
@@ -1,18 +1,11 @@
 import java.util.*;
 
 public class Permutations {
-	public static long nextPermutation(long x) {
-		long s = x & -x;
-		long r = x + s;
-		long ones = x ^ r;
-		ones = (ones >> 2) / s;
-		return r | ones;
-	}
 
 	public static boolean nextPermutation(int[] p) {
 		for (int a = p.length - 2; a >= 0; --a)
 			if (p[a] < p[a + 1])
-				for (int b = p.length - 1;; --b)
+				for (int b = p.length - 1; ; --b)
 					if (p[b] > p[a]) {
 						int t = p[a];
 						p[a] = p[b];
@@ -27,22 +20,26 @@ public class Permutations {
 		return false;
 	}
 
-	public static void generatePermutations(int[] p, int depth) {
-		int n = p.length;
-		if (depth == n) {
-			System.out.println(Arrays.toString(p));
-			return;
+	public static int[] permutationByNumber(int len, long number) {
+		long[] fact = new long[len];
+		fact[0] = 1;
+		for (int i = 1; i < len; i++) {
+			fact[i] = i * fact[i - 1];
 		}
-		for (int i = 0; i < n; i++) {
-			if (p[i] == 0) {
-				p[i] = depth;
-				generatePermutations(p, depth + 1);
-				p[i] = 0;
+		int[] p = new int[len];
+		for (int i = 0; i < len; i++) {
+			p[i] = (int) (number / fact[len - 1 - i]);
+			number %= fact[len - 1 - i];
+			for (int j = 0; j < i; j++) {
+				if (p[j] <= p[i]) {
+					++p[i];
+				}
 			}
 		}
+		return p;
 	}
 
-	public static long permNumber(int[] p) {
+	public static long numberByPermutation(int[] p) {
 		int n = p.length;
 		long[] fact = new long[n];
 		fact[0] = 1;
@@ -60,6 +57,29 @@ public class Permutations {
 			res += a * fact[n - 1 - i];
 		}
 		return res;
+	}
+
+	public static void generatePermutations(int[] p, int depth) {
+		int n = p.length;
+		if (depth == n) {
+			System.out.println(Arrays.toString(p));
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (p[i] == 0) {
+				p[i] = depth;
+				generatePermutations(p, depth + 1);
+				p[i] = 0;
+			}
+		}
+	}
+
+	public static long nextPermutation(long x) {
+		long s = x & -x;
+		long r = x + s;
+		long ones = x ^ r;
+		ones = (ones >> 2) / s;
+		return r | ones;
 	}
 
 	public static List<List<Integer>> decomposeIntoCycles(int[] p) {
@@ -83,19 +103,21 @@ public class Permutations {
 
 	// Usage example
 	public static void main(String[] args) {
-		// print all permutations method1
+		// print all permutations method 1
 		generatePermutations(new int[2], 1);
-		// print all permutations method2
-		int[] p = { 1, 2, 3 };
+
+		// print all permutations method 2
+		int[] p = {1, 2, 3};
 		do {
 			System.out.println(Arrays.toString(p));
 		} while (nextPermutation(p));
 
-		long x = Long.valueOf("1011", 2);
-		long y = Long.valueOf("1101", 2);
-		System.out.println(true == (y == nextPermutation(x)));
-
-		System.out.println(4 == permNumber(new int[] { 2, 0, 1 }));
-		System.out.println(decomposeIntoCycles(new int[] { 0, 2, 1, 3 }));
+		p = new int[]{2, 0, 1};
+		System.out.println(4 == numberByPermutation(p));
+		System.out.println(Arrays.equals(p, permutationByNumber(p.length, numberByPermutation(p))));
+		int x = 7;
+		System.out.println(x == numberByPermutation(permutationByNumber(3, x)));
+		System.out.println(0b1101 == nextPermutation(0b1011));
+		System.out.println(decomposeIntoCycles(new int[]{0, 2, 1, 3}));
 	}
 }
