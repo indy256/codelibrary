620b6f1c88fc460fff2424a1a76e621748d96b9a
Andrey Naumenko <indy256@gmail.com>
2014-03-01 06:11:06 +0300

    update

--- java/src/Trie.java
+++ java/src/Trie.java
@@ -1,25 +1,25 @@
-import java.util.*;
-
 public class Trie {
 	static class TrieNode {
-		Map<Character, TrieNode> children = new TreeMap<>();
+		TrieNode[] children = new TrieNode[26];
 		boolean leaf;
 	}
 
 	public static void insertString(TrieNode root, String s) {
 		TrieNode v = root;
 		for (char ch : s.toCharArray()) {
-			TrieNode next = v.children.get(ch);
+			TrieNode next = v.children[ch - 'a'];
 			if (next == null)
-				v.children.put(ch, next = new TrieNode());
+				v.children[ch - 'a'] = next = new TrieNode();
 			v = next;
 		}
 		v.leaf = true;
 	}
 
 	public static void printSorted(TrieNode node, String s) {
-		for (Character ch : node.children.keySet()) {
-			printSorted(node.children.get(ch), s + ch);
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			TrieNode child = node.children[ch - 'a'];
+			if (child != null)
+				printSorted(child, s + ch);
 		}
 		if (node.leaf) {
 			System.out.println(s);
