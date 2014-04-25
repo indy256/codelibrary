a7055d9b180e9f3e2a47389486950e42496d2480
Andrey Naumenko <indy256@gmail.com>
2014-04-25 14:43:42 +0300

    update

--- java/src/MaxMatching2.java
+++ java/src/MaxMatching2.java
@@ -2,18 +2,6 @@ import java.util.*;
 
 public class MaxMatching2 {
 
-	static boolean findPath(List<Integer>[] graph, int u1, int[] matching, boolean[] vis) {
-		vis[u1] = true;
-		for (int v : graph[u1]) {
-			int u2 = matching[v];
-			if (u2 == -1 || !vis[u2] && findPath(graph, u2, matching, vis)) {
-				matching[v] = u1;
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public static int maxMatching(List<Integer>[] graph, int n2) {
 		int n1 = graph.length;
 		int[] matching = new int[n2];
@@ -26,17 +14,51 @@ public class MaxMatching2 {
 		return matches;
 	}
 
-	// Usage example
-	public static void main(String[] args) {
-		int n1 = 2;
-		int n2 = 3;
-		List<Integer>[] g = new List[n1];
-		for (int i = 0; i < n1; i++) {
-			g[i] = new ArrayList<>();
+	static boolean findPath(List<Integer>[] graph, int u1, int[] matching, boolean[] vis) {
+		vis[u1] = true;
+		for (int v : graph[u1]) {
+			int u2 = matching[v];
+			if (u2 == -1 || !vis[u2] && findPath(graph, u2, matching, vis)) {
+				matching[v] = u1;
+				return true;
+			}
 		}
-		g[0].add(2);
-		g[0].add(0);
-		g[1].add(2);
-		System.out.println(2 == maxMatching(g, n2));
+		return false;
+	}
+
+	// random tests
+	public static void main(String[] args) {
+		Random rnd = new Random(1);
+		for (int step = 0; step < 1000; step++) {
+			int n1 = rnd.nextInt(20) + 1;
+			int n2 = rnd.nextInt(20) + 1;
+			List<Integer>[] g = new List[n1];
+			for (int i = 0; i < n1; i++)
+				g[i] = new ArrayList<>();
+			for (int i = 0; i < n1; i++)
+				for (int j = 0; j < n2; j++)
+					g[i].add(j);
+			int res1 = maxMatching(g, n2);
+			int res2 = slowMinVertexCover(g, n2);
+			if (res1 != res2)
+				throw new RuntimeException();
+		}
+	}
+
+	static int slowMinVertexCover(List<Integer>[] g, int n2) {
+		int n1 = g.length;
+		int[] mask = new int[n1];
+		for (int i = 0; i < n1; i++)
+			for (int j : g[i])
+				mask[i] |= 1 << j;
+		int res = n2;
+		for (int m = 0; m < 1 << n2; m++) {
+			int cur = Integer.bitCount(m);
+			for (int i = 0; i < n1; i++)
+				if ((mask[i] & m) != mask[i])
+					++cur;
+			res = Math.min(res, cur);
+		}
+		return res;
 	}
 }
