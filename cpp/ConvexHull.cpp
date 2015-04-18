1b3f9c1c35adbea2c15f7d6cc8eacf2d95644446
Andrey Naumenko <indy256@gmail.com>
2015-04-19 02:15:55 +0300

    update c++

--- cpp/ConvexHull.cpp
+++ cpp/ConvexHull.cpp
@@ -1,37 +1,32 @@
 #include <algorithm>
-#include <vector>
 #include <iostream>
+
 using namespace std;
 
-typedef pair<long, long> point;
+typedef pair<long long, long long> point;
 
-bool cw(const point &a, const point &b, const point &c) {
-    return (b.first - a.first) * (c.second - a.second) - (b.second - a.second) * (c.first - a.first) < 0;
+long long cross(const point &a, const point &b, const point &c) {
+	return (b.first - a.first) * (c.second - a.second) - (b.second - a.second) * (c.first - a.first);
 }
 
 vector<point> convexHull(vector<point> p) {
-    int n = p.size();
-    if (n <= 1)
-        return p;
-    sort(p.begin(), p.end());
-    int cnt = 0;
-    vector<point> q(n * 2);
-    for (int i = 0; i < n; q[cnt++] = p[i++])
-        for (; cnt >= 2 && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
-            ;
-    for (int i = n - 2, t = cnt; i >= 0; q[cnt++] = p[i--])
-        for (; cnt > t && !cw(q[cnt - 2], q[cnt - 1], p[i]); --cnt)
-            ;
-    q.resize(cnt - 1 - (q[0] == q[1]));
-    return q;
+	int n = p.size();
+	if (n <= 1)
+		return p;
+	sort(p.begin(), p.end());
+	vector<point> q;
+	for (int i = 0; i < n; q.push_back(p[i++]))
+		while (q.size() >= 2 && cross(q.end()[-2], q.back(), p[i]) >= 0)
+			q.pop_back();
+	for (int i = n - 2, t = q.size(); i >= 0; q.push_back(p[i--]))
+		while (q.size() > t && cross(q.end()[-2], q.back(), p[i]) >= 0)
+			q.pop_back();
+	q.resize(q.size() - 1 - (q[0] == q[1]));
+	return q;
 }
 
 int main() {
-    vector<point> points(4);
-    points[0] = point(0, 0);
-    points[1] = point(3, 0);
-    points[2] = point(0, 3);
-    points[3] = point(1, 1);
-    vector<point> hull = convexHull(points);
-    cout << (3 == hull.size()) << endl;
+	vector<point> points = {point(0, 0), point(3, 0), point(0, 3), point(1, 1)};
+	vector<point> hull = convexHull(points);
+	cout << (3 == hull.size()) << endl;
 }
