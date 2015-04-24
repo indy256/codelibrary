e6786026b06d77a019865f86a05d1964166b6669
Andrey Naumenko <indy256@gmail.com>
2015-04-25 02:14:58 +0300

    update

--- java/src/SuffixArray.java
+++ java/src/SuffixArray.java
@@ -1,23 +1,17 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class SuffixArray {
 
 	// sort suffixes of S in O(n*log(n))
 	public static int[] suffixArray(CharSequence S) {
 		int n = S.length();
-		Integer[] order = new Integer[n];
-		for (int i = 0; i < n; i++)
-			order[i] = n - 1 - i;
 
 		// stable sort of characters
-		Arrays.sort(order, (a, b) -> Character.compare(S.charAt(a), S.charAt(b)));
+		int[] sa = IntStream.range(0, n).mapToObj(i -> n - 1 - i).
+				sorted((a, b) -> Character.compare(S.charAt(a), S.charAt(b))).mapToInt(Integer::intValue).toArray();
 
-		int[] sa = new int[n];
-		int[] classes = new int[n];
-		for (int i = 0; i < n; i++) {
-			sa[i] = order[i];
-			classes[i] = S.charAt(i);
-		}
+		int[] classes = S.chars().toArray();
 		// sa[i] - suffix on i'th position after sorting by first len characters
 		// classes[i] - equivalence class of the i'th suffix after sorting by first len characters
 
@@ -30,9 +24,7 @@ public class SuffixArray {
 			}
 			// Suffixes are already sorted by first len characters
 			// Now sort suffixes by first len * 2 characters
-			int[] cnt = new int[n];
-			for (int i = 0; i < n; i++)
-				cnt[i] = i;
+			int[] cnt = IntStream.range(0, n).toArray();
 			int[] s = sa.clone();
 			for (int i = 0; i < n; i++) {
 				// s[i] - order of suffixes sorted by first len characters
@@ -49,23 +41,14 @@ public class SuffixArray {
 	// sort rotations of S in O(n*log(n))
 	public static int[] rotationArray(CharSequence S) {
 		int n = S.length();
-		Integer[] order = new Integer[n];
-		for (int i = 0; i < n; i++)
-			order[i] = i;
-		Arrays.sort(order, (a, b) -> Character.compare(S.charAt(a), S.charAt(b)));
-		int[] sa = new int[n];
-		int[] classes = new int[n];
-		for (int i = 0; i < n; i++) {
-			sa[i] = order[i];
-			classes[i] = S.charAt(i);
-		}
+		int[] sa = IntStream.range(0, n).mapToObj(Integer::valueOf).
+				sorted((a, b) -> Character.compare(S.charAt(a), S.charAt(b))).mapToInt(Integer::intValue).toArray();
+		int[] classes = S.chars().toArray();
 		for (int len = 1; len < n; len *= 2) {
 			int[] c = classes.clone();
 			for (int i = 0; i < n; i++)
 				classes[sa[i]] = i > 0 && c[sa[i - 1]] == c[sa[i]] && c[(sa[i - 1] + len / 2) % n] == c[(sa[i] + len / 2) % n] ? classes[sa[i - 1]] : i;
-			int[] cnt = new int[n];
-			for (int i = 0; i < n; i++)
-				cnt[i] = i;
+			int[] cnt = IntStream.range(0, n).toArray();
 			int[] s = sa.clone();
 			for (int i = 0; i < n; i++) {
 				int s1 = (s[i] - len + n) % n;
@@ -109,9 +92,7 @@ public class SuffixArray {
 		Random rnd = new Random(1);
 		for (int step = 0; step < 100000; step++) {
 			int n = rnd.nextInt(100) + 1;
-			StringBuilder s = new StringBuilder();
-			for (int i = 0; i < n; i++)
-				s.append((char) ('\1' + rnd.nextInt(10)));
+			StringBuilder s = rnd.ints(n, 0, 10).collect(StringBuilder::new, (sb, i) -> sb.append((char) ('\1' + i)), StringBuilder::append);
 			int[] sa = suffixArray(s);
 			int[] ra = rotationArray(s.toString() + '\0');
 			int[] lcp = lcp(sa, s);
--- java/src/SuffixArray2.java
+++ java/src/SuffixArray2.java
@@ -1,16 +1,14 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class SuffixArray2 {
 
 	// suffix array in O(n*log^2(n))
-	public static Integer[] suffixArray(CharSequence s) {
+	public static int[] suffixArray(CharSequence s) {
 		int n = s.length();
-		Integer[] sa = new Integer[n];
-		int[] rank = new int[n];
-		for (int i = 0; i < n; i++) {
-			sa[i] = i;
-			rank[i] = s.charAt(i);
-		}
+		Integer[] sa = IntStream.range(0, n).mapToObj(Integer::valueOf).toArray(Integer[]::new);
+		int[] rank = s.chars().toArray();
+
 		for (int len = 1; len < n; len *= 2) {
 			long[] rank2 = new long[n];
 			for (int i = 0; i < n; i++)
@@ -21,7 +19,7 @@ public class SuffixArray2 {
 			for (int i = 0; i < n; i++)
 				rank[sa[i]] = i > 0 && rank2[sa[i - 1]] == rank2[sa[i]] ? rank[sa[i - 1]] : i;
 		}
-		return sa;
+		return Arrays.stream(sa).mapToInt(Integer::intValue).toArray();
 	}
 
 	// random test
@@ -29,10 +27,8 @@ public class SuffixArray2 {
 		Random rnd = new Random(1);
 		for (int step = 0; step < 100000; step++) {
 			int n = rnd.nextInt(100);
-			StringBuilder s = new StringBuilder();
-			for (int i = 0; i < n; i++)
-				s.append((char) ('\0' + rnd.nextInt(10)));
-			Integer[] sa = suffixArray(s);
+			StringBuilder s = rnd.ints(n, 0, 10).collect(StringBuilder::new, (sb, i) -> sb.append((char) ('\1' + i)), StringBuilder::append);
+			int[] sa = suffixArray(s);
 			for (int i = 0; i + 1 < n; i++)
 				if (s.substring(sa[i]).compareTo(s.substring(sa[i + 1])) >= 0)
 					throw new RuntimeException();
