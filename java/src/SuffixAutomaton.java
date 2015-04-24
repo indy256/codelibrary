5c56f0e9fb98962e8146143319e9e7d06cfb5a8f
Andrey Naumenko <indy256@gmail.com>
2015-04-24 23:31:51 +0300

    update

--- java/src/SuffixAutomaton.java
+++ java/src/SuffixAutomaton.java
@@ -63,20 +63,45 @@ public class SuffixAutomaton {
 		Random rnd = new Random(1);
 		for (int step = 0; step < 100_000; step++) {
 			int len1 = rnd.nextInt(20);
-			int len2 = rnd.nextInt(20) + 1;
+			int len2 = rnd.nextInt(5) + 1;
 			String a = getRandomString(len1, rnd);
 			String b = getRandomString(len2, rnd);
-			String res1 = lcs(a, b);
-			int res2 = slowLcs(a, b);
-			if (res1.length() != res2)
-				throw new RuntimeException();
 			int[] occurrences1 = occurrences(a, b);
 			List<Integer> occurrences2 = new ArrayList<>();
 			for (int p = a.indexOf(b); p != -1; p = a.indexOf(b, p + 1))
 				occurrences2.add(p);
 			if (!Arrays.equals(occurrences1, occurrences2.stream().mapToInt(Integer::intValue).toArray()))
 				throw new RuntimeException();
+			String res1 = lcs(a, b);
+			int res2 = slowLcs(a, b);
+			if (res1.length() != res2)
+				throw new RuntimeException();
 		}
+	}
+
+	static int[] occurrences(String haystack, String needle) {
+		State[] automaton = buildSuffixAutomaton(haystack);
+		int node = 0;
+		for (char c : needle.toCharArray()) {
+			int next = automaton[node].next[c];
+			if (next == -1) {
+				return new int[0];
+			}
+			node = next;
+		}
+		List<Integer> occurrences = new ArrayList<>();
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(node);
+		while (!q.isEmpty()) {
+			int curNode = q.remove();
+			if (automaton[curNode].firstPos != -1) {
+				occurrences.add(automaton[curNode].firstPos - needle.length() + 1);
+			}
+			for (int nextNode : automaton[curNode].invSuffLinks) {
+				q.add(nextNode);
+			}
+		}
+		return occurrences.stream().mapToInt(Integer::intValue).sorted().toArray();
 	}
 
 	static String lcs(String a, String b) {
@@ -106,28 +131,6 @@ public class SuffixAutomaton {
 			}
 		}
 		return b.substring(bestPos - bestLen + 1, bestPos + 1);
-	}
-
-	static int[] occurrences(String haystack, String needle) {
-		State[] automaton = buildSuffixAutomaton(haystack);
-		int node = 0;
-		for (char c : needle.toCharArray()) {
-			int next = automaton[node].next[c];
-			if (next == -1) {
-				return new int[0];
-			}
-			node = next;
-		}
-		List<Integer> occurrences = new ArrayList<>();
-		collectOccurrences(automaton, node, needle.length(), occurrences);
-		return occurrences.stream().mapToInt(Integer::intValue).sorted().toArray();
-	}
-
-	static void collectOccurrences(State[] automaton, int curNode, int searchLength, List<Integer> occurrences) {
-		if (automaton[curNode].firstPos != -1)
-			occurrences.add(automaton[curNode].firstPos - searchLength + 1);
-		for (int nextNode : automaton[curNode].invSuffLinks)
-			collectOccurrences(automaton, nextNode, searchLength, occurrences);
 	}
 
 	static int slowLcs(String a, String b) {
