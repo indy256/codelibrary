994de3cc8b82c6f8b7fba4f6994179baa267bb1a
indy256 <indy256@gmail.com>
2015-09-21 01:11:52 +0300

    update

--- haskell/quicksort.hs
+++ haskell/quicksort.hs
@@ -1,5 +1,5 @@
 quicksort [] = []
-quicksort (p:a) = quicksort (filter (<= p) a) ++ [p] ++ quicksort (filter (> p) a)
+quicksort (p:xs) = quicksort (filter (<= p) xs) ++ [p] ++ quicksort (filter (> p) xs)
 
 -- Usage example
 main = print $ quicksort [2,3,1]
