1db1b10bd6775b6eafae3590d54ffb66e50a39ab
indy256 <indy256@gmail.com>
2016-03-13 01:30:41 +0300

    update

--- java/src/Partition.java
+++ java/src/Partition.java
@@ -1,58 +1,76 @@
-import java.util.*;
+import java.util.Random;
 
 public class Partition {
 
 	static Random rnd = new Random(1);
 
-	public static int randomizedPartition(int[] a, int low, int high) {
-		swap(a, low + rnd.nextInt(high - low), high - 1);
-		int separator = a[high - 1];
-		int i = low - 1;
-		for (int j = low; j < high; j++) {
-			if (a[j] <= separator) {
-				swap(a, ++i, j);
-			}
-		}
-		return i;
+	public static int randomizedPartition(int[] a, int i, int j) {
+		int high = j;
+		int separator = a[i + rnd.nextInt(j - i + 1)];
+		do {
+			while (a[i] < separator)
+				++i;
+			while (a[j] > separator)
+				--j;
+			if (i > j)
+				break;
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+			++i;
+			--j;
+		} while (i <= j);
+		return Math.max(0, j);
 	}
 
-	public static int partition(int[] a, int low, int high) {
-		swap(a, (low + high) >>> 1, high - 1);
-		int separator = a[high - 1];
-		int i = low - 1;
-		for (int j = low; j < high; j++) {
-			if (a[j] <= separator) {
-				swap(a, ++i, j);
-			}
-		}
-		return i;
-	}
+	public static int partition(int[] a, int fromInclusive, int toExclusive, int separatorIndex) {
+		int i = fromInclusive;
+		int j = toExclusive - 1;
+		if (i >= j)
+			return j;
 
-	static void swap(int[] a, int i, int j) {
-		int t = a[i];
-		a[i] = a[j];
-		a[j] = t;
+		int separator = a[separatorIndex];
+		a[separatorIndex] = a[i];
+		a[i] = separator;
+		++i;
+		while (i <= j) {
+			while (i <= j && a[i] < separator)
+				++i;
+			while (i <= j && a[j] > separator)
+				--j;
+			if (i >= j)
+				break;
+			int t = a[j];
+			a[j] = a[i];
+			a[i] = t;
+			++i;
+			--j;
+		}
+		a[fromInclusive] = a[j];
+		a[j] = separator;
+		return j;
 	}
 
 	// Random test
 	public static void main(String[] args) {
 		for (int step = 0; step < 100_000; step++) {
-			int n = rnd.nextInt(10) + 1;
-			int[] a = new int[n];
-			for (int i = 0; i < n; i++)
-				a[i] = rnd.nextInt(10);
-			int[] a1 = a.clone();
-			check(a1, randomizedPartition(a1, 0, n));
-			int[] a2 = a.clone();
-			check(a2, partition(a2, 0, n));
+			int n = rnd.nextInt(10) + 2;
+			int[] a = rnd.ints(n, 0, 10).toArray();
+			int[] b = a.clone();
+			check(a, randomizedPartition(a, 0, n - 1), n - 2);
+			for (int i = 0; i < n; i++) {
+				int[] c = b.clone();
+				check(c, partition(c, 0, n, i), n - 1);
+			}
 		}
 	}
 
-	static void check(int[] a, int k) {
-		if (k < 0 || k >= a.length)
+	static void check(int[] a, int k, int maxpos) {
+		if (k < 0 || k > maxpos)
 			throw new RuntimeException();
-		for (int i = 0; i < a.length; i++)
-			if (i < k && a[i] > a[k] || i > k && a[i] < a[k])
-				throw new RuntimeException();
+		for (int i = 0; i < k; i++)
+			for (int j = k + 1; j < a.length; j++)
+				if (a[i] > a[j])
+					throw new RuntimeException();
 	}
 }
