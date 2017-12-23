a795405fee09ff40c49b51406e0b3d85ed94c354
indy256 <indy256@gmail.com>
2017-12-24 00:09:44 +0100

    update

--- kotlin/src/ConvexHull.kt
+++ kotlin/src/ConvexHull.kt
@@ -16,13 +16,13 @@ object ConvexHull {
         val sortedPoints = points.sortedArrayWith(compareBy({ it.x }, { it.y }))
         val n = sortedPoints.size
         val hull = arrayListOf<Point>()
-        for (i in 0..2 * n - 1) {
+        for (i in 0 until 2 * n) {
             val j = if (i < n) i else 2 * n - 1 - i
             while (hull.size >= 2 && isNotRightTurn(hull.takeLast(2) + sortedPoints[j]))
                 hull.removeAt(hull.size - 1)
             hull.add(sortedPoints[j])
         }
-        return hull.toTypedArray()
+        return hull.subList(0, hull.size - 1).toTypedArray()
     }
 
     // Usage example
@@ -30,6 +30,6 @@ object ConvexHull {
     fun main(args: Array<String>) {
         val points = arrayOf(Point(0, 0), Point(2, 2), Point(1, 1), Point(0, 1), Point(1, 0), Point(0, 0))
         val hull = convexHull(points)
-        println(Arrays.toString(hull))
+        println(hull.contentToString())
     }
 }
