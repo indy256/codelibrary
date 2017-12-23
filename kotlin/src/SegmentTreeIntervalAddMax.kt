75d06b4b0464e3d1c8258ac9b1fe2835c0a66cbd
indy256 <indy256@gmail.com>
2017-12-23 01:42:40 +0100

    update

--- kotlin/src/SegmentTreeIntervalAddMax.kt
+++ kotlin/src/SegmentTreeIntervalAddMax.kt
@@ -2,39 +2,39 @@ class SegmentTreeIntervalAddMax(val n: Int) {
     val tmax = IntArray(4 * n)
     val tadd = IntArray(4 * n) // tadd[i] applies to tmax[i], tadd[2*i+1] and tadd[2*i+2]
 
-    fun max(from: Int, to: Int, root: Int = 0, left: Int = 0, right: Int = n - 1): Int {
+    fun max(from: Int, to: Int, node: Int = 0, left: Int = 0, right: Int = n - 1): Int {
         if (from == left && to == right) {
-            return tmax[root] + tadd[root]
+            return tmax[node] + tadd[node]
         }
-        push(root)
+        push(node)
         val mid = left + right shr 1
         var res = Int.MIN_VALUE
         if (from <= mid)
-            res = maxOf(res, max(from, minOf(to, mid), 2 * root + 1, left, mid))
+            res = maxOf(res, max(from, minOf(to, mid), 2 * node + 1, left, mid))
         if (to > mid)
-            res = maxOf(res, max(maxOf(from, mid + 1), to, 2 * root + 2, mid + 1, right))
+            res = maxOf(res, max(maxOf(from, mid + 1), to, 2 * node + 2, mid + 1, right))
         return res
     }
 
-    fun add(from: Int, to: Int, delta: Int, root: Int = 0, left: Int = 0, right: Int = n - 1) {
+    fun add(from: Int, to: Int, delta: Int, node: Int = 0, left: Int = 0, right: Int = n - 1) {
         if (from == left && to == right) {
-            tadd[root] += delta
+            tadd[node] += delta
             return
         }
-        push(root) // this push may be omitted for add, but is necessary for other operations such as set
+        push(node) // this push may be omitted for add, but is necessary for other operations such as set
         val mid = left + right shr 1
         if (from <= mid)
-            add(from, minOf(to, mid), delta, 2 * root + 1, left, mid)
+            add(from, minOf(to, mid), delta, 2 * node + 1, left, mid)
         if (to > mid)
-            add(maxOf(from, mid + 1), to, delta, 2 * root + 2, mid + 1, right)
-        tmax[root] = maxOf(tmax[2 * root + 1] + tadd[2 * root + 1], tmax[2 * root + 2] + tadd[2 * root + 2])
+            add(maxOf(from, mid + 1), to, delta, 2 * node + 2, mid + 1, right)
+        tmax[node] = maxOf(tmax[2 * node + 1] + tadd[2 * node + 1], tmax[2 * node + 2] + tadd[2 * node + 2])
     }
 
-    fun push(root: Int) {
-        tmax[root] += tadd[root]
-        tadd[2 * root + 1] += tadd[root]
-        tadd[2 * root + 2] += tadd[root]
-        tadd[root] = 0
+    fun push(node: Int) {
+        tmax[node] += tadd[node]
+        tadd[2 * node + 1] += tadd[node]
+        tadd[2 * node + 2] += tadd[node]
+        tadd[node] = 0
     }
 }
 
