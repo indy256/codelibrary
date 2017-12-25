3f353be7b9cc010ddac141c1afc9f4520719b355
indy256 <indy256@gmail.com>
2017-12-25 19:47:43 +0100

    update

--- kotlin/src/Gauss.kt
+++ kotlin/src/Gauss.kt
@@ -1,4 +1,7 @@
+import java.util.Random
+
 // https://en.wikipedia.org/wiki/Gauss–Jordan_elimination
+// returns X such that A * X = B
 fun gauss(A: Array<DoubleArray>, B: DoubleArray): DoubleArray {
     val a = A.map { it.copyOf() }.toTypedArray()
     val b = B.copyOf()
@@ -7,31 +10,37 @@ fun gauss(A: Array<DoubleArray>, B: DoubleArray): DoubleArray {
         val best = (row until n).maxBy { Math.abs(a[it][row]) }!!
         a[row] = a[best].also { a[best] = a[row] }
         b[row] = b[best].also { b[best] = b[row] }
-        for (i in row + 1 until n)
-            a[row][i] /= a[row][row]
+        for (j in row + 1 until n)
+            a[row][j] /= a[row][row]
         b[row] /= a[row][row]
         // a[row][row] = 1;
-        for (i in 0 until n) {
-            val z = a[i][row]
-            if (i != row && z != 0.0) {
+        for (j in 0 until n) {
+            val z = a[j][row]
+            if (j != row && z != 0.0) {
                 // row + 1 instead of row is an optimization
-                for (j in row + 1 until n)
-                    a[i][j] -= a[row][j] * z
-                b[i] -= b[row] * z
+                for (k in row + 1 until n)
+                    a[j][k] -= a[row][k] * z
+                b[j] -= b[row] * z
             }
         }
     }
     return b
 }
 
-// Usage example
+// random test
 fun main(args: Array<String>) {
-    val a = arrayOf(doubleArrayOf(4.0, 2.0, -1.0), doubleArrayOf(2.0, 4.0, 3.0), doubleArrayOf(-1.0, 3.0, 5.0))
-    val b = doubleArrayOf(1.0, 0.0, 0.0)
-    val x = gauss(a, b)
-    for (i in a.indices) {
-        val y = a[i].zip(x).map { it -> it.first * it.second }.sum()
-        if (Math.abs(b[i] - y) > 1e-9)
-            throw RuntimeException()
+    val rnd = Random(1)
+    for (step in 0 until 10_000) {
+        val n = rnd.nextInt(5) + 1
+        val a = (0 until n).map { (0 until n).map { (rnd.nextInt(10) - 5).toDouble() }.toDoubleArray() }.toTypedArray()
+        if (Math.abs(det(a)) > 1e-6) {
+            val b = (0 until n).map { (rnd.nextInt(10) - 5).toDouble() }.toDoubleArray()
+            val x = gauss(a, b)
+            for (i in a.indices) {
+                val y = a[i].zip(x).map { it -> it.first * it.second }.sum()
+                if (Math.abs(b[i] - y) > 1e-9)
+                    throw RuntimeException()
+            }
+        }
     }
 }
