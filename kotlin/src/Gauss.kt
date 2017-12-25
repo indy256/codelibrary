26d972af8b156dc2c182896f2ec2ce7cf0709709
indy256 <indy256@gmail.com>
2017-12-25 23:26:15 +0100

    update

--- kotlin/src/Gauss.kt
+++ kotlin/src/Gauss.kt
@@ -2,7 +2,7 @@ import java.util.Random
 
 // https://en.wikipedia.org/wiki/Gauss–Jordan_elimination
 // returns x such that A * x = b
-fun gauss2(A: Array<DoubleArray>, b: DoubleArray): DoubleArray {
+fun gauss(A: Array<DoubleArray>, b: DoubleArray): DoubleArray {
     val a = A.mapIndexed { i, Ai -> Ai + b[i] }.toTypedArray()
     val n = a.size
     for (i in 0 until n) {
@@ -29,7 +29,7 @@ fun main(args: Array<String>) {
         val a = (0 until n).map { (0 until n).map { (rnd.nextInt(10) - 5).toDouble() }.toDoubleArray() }.toTypedArray()
         if (Math.abs(det(a)) > 1e-6) {
             val b = (0 until n).map { (rnd.nextInt(10) - 5).toDouble() }.toDoubleArray()
-            val x = gauss2(a, b)
+            val x = gauss(a, b)
             for (i in a.indices) {
                 val y = a[i].zip(x).map { it -> it.first * it.second }.sum()
                 if (Math.abs(b[i] - y) > 1e-9)
