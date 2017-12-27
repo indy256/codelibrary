7b8efffb7c1b201ef36a4c8087feba9faf8540f2
indy256 <indy256@gmail.com>
2017-12-27 01:23:12 +0100

    update

--- cpp/Prim.cpp
+++ cpp/Prim.cpp
@@ -2,54 +2,51 @@
 #include <queue>
 #include <climits>
 #include <iostream>
+#include <tuple>
+
 using namespace std;
 
-typedef pair<int, int> pii;
-typedef vector<vector<pii> > Graph;
+typedef pair<int, int> edge;
 
-long long prim(Graph &g, vector<int> &pred) {
-    int n = g.size();
-    pred.assign(n, -1);
-    vector<bool> vis(n);
+auto prim(vector<vector<edge>> &g) {
+    size_t n = g.size();
+    vector<bool> used(n);
+    vector<int> pred(n, -1);
     vector<int> prio(n, INT_MAX);
     prio[0] = 0;
-    priority_queue<pii, vector<pii> , greater<pii> > q;
-    q.push(make_pair(prio[0] , 0));
-    long long res = 0;
+    priority_queue<edge, vector<edge>, greater<edge>> q;
+    q.push({prio[0], 0});
+    long long tree_weight = 0;
 
     while (!q.empty()) {
-        int d = q.top().first;
-        int u = q.top().second;
+        auto[w, u] = q.top();
         q.pop();
-        if (vis[u])
+
+        if (used[u])
             continue;
-        vis[u] = true;
-        res += d;
-        for (int i = 0; i < (int) g[u].size(); i++) {
-            int v = g[u][i].first;
-            if (vis[v])
-                continue;
-            int nprio = g[u][i].second;
-            if (prio[v] > nprio) {
-                prio[v] = nprio;
+        used[u] = true;
+        tree_weight += w;
+
+        for (auto[v, weight] : g[u]) {
+            if (!used[v] && prio[v] > weight) {
+                prio[v] = weight;
                 pred[v] = u;
-                q.push(make_pair(nprio, v));
+                q.push({weight, v});
             }
         }
     }
-    return res;
+    return make_tuple(tree_weight, pred);
 }
 
 int main() {
-    Graph g(3);
-    g[0].push_back(make_pair(1, 10));
-    g[1].push_back(make_pair(0, 10));
-    g[1].push_back(make_pair(2, 10));
-    g[2].push_back(make_pair(1, 10));
-    g[2].push_back(make_pair(0, 5));
-    g[0].push_back(make_pair(2, 5));
+    vector<vector<edge>> g(3);
+    g[0].push_back({1, 10});
+    g[1].push_back({0, 10});
+    g[1].push_back({2, 10});
+    g[2].push_back({1, 10});
+    g[2].push_back({0, 5});
+    g[0].push_back({2, 5});
 
-    vector<int> pred;
-    long long res = prim(g, pred);
-    cout << res << endl;
+    auto[len, pred] = prim(g);
+    cout << len << endl;
 }
