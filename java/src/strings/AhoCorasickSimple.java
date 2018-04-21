13598cfe29d52a616afa3dd8da5e6d01a4884021
indy256 <indy256@gmail.com>
2018-04-21 02:22:39 +0200

    update

--- java/src/strings/AhoCorasickSimple.java
+++ java/src/strings/AhoCorasickSimple.java
@@ -4,35 +4,43 @@ import java.util.*;
 
 public class AhoCorasickSimple {
 
-	static final int ALPHABET_SIZE = 26;
+    static final int ALPHABET_SIZE = 26;
 
-	String[] prefixes;
+    String[] prefixes;
 
-	public int[][] buildAutomata(String[] words) {
-		Map<String, Integer> prefixMap = new TreeMap<>();
-		for (String s : words) {
-			for (int i = 0; i <= s.length(); i++) {
-				prefixMap.put(s.substring(0, i), 0);
-			}
-		}
-		prefixes = prefixMap.keySet().toArray(new String[0]);
-		for (int i = 0; i < prefixes.length; i++) {
-			prefixMap.put(prefixes[i], i);
-		}
-		int[][] transitions = new int[prefixes.length][ALPHABET_SIZE];
-		for (int i = 0; i < prefixes.length; i++) {
-			for (int j = 0; j < ALPHABET_SIZE; j++) {
-				String s = prefixes[i] + (char) ('a' + j);
-				while (!prefixMap.containsKey(s)) {
-					s = s.substring(1);
-				}
-				transitions[i][j] = prefixMap.get(s);
-			}
-		}
-		return transitions;
-	}
+    public int[][] buildAutomata(String[] words) {
+        Map<String, Integer> prefixMap = new TreeMap<>();
+        for (String s : words) {
+            for (int i = 0; i <= s.length(); i++) {
+                prefixMap.put(s.substring(0, i), 0);
+            }
+        }
+        prefixes = prefixMap.keySet().toArray(new String[0]);
+        for (int i = 0; i < prefixes.length; i++) {
+            prefixMap.put(prefixes[i], i);
+        }
+        int[][] transitions = new int[prefixes.length][ALPHABET_SIZE];
+        for (int i = 0; i < prefixes.length; i++) {
+            for (int j = 0; j < ALPHABET_SIZE; j++) {
+                String s = prefixes[i] + (char) ('a' + j);
+                while (!prefixMap.containsKey(s)) {
+                    s = s.substring(1);
+                }
+                transitions[i][j] = prefixMap.get(s);
+            }
+        }
+        return transitions;
+    }
 
-	// Usage example
-	public static void main(String[] args) {
-	}
+    // Usage example
+    public static void main(String[] args) {
+        AhoCorasickSimple automaton = new AhoCorasickSimple();
+        int[][] transitions = automaton.buildAutomata(new String[]{"abc", "bc"});
+        String s = "tabcbc";
+        int state = 0;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.substring(0, i) + " " + automaton.prefixes[state]);
+            state = transitions[state][s.charAt(i) - 'a'];
+        }
+    }
 }
