5be8c1d6c7d65c4865fca30e3de6006db36c598d
anavumenka <andrei.navumenka@zalando.de>
2018-05-15 17:53:54 +0200

    update

--- cpp/graphs/tree_isomorphism.cpp
+++ cpp/graphs/tree_isomorphism.cpp
@@ -1,40 +1,30 @@
-#include <algorithm>
-#include <vector>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-
-vvi children, subtreeLabels, tree, L;
-vi pred, map;
+vector<vector<int>> children, subtreeLabels, tree, L;
+vector<int> pred, mapping;
 int n;
 
-bool compare(int a, int b) {
-    return subtreeLabels[a] < subtreeLabels[b];
-}
+auto compare = [](int a, int b) -> bool { return subtreeLabels[a] < subtreeLabels[b]; };
+auto equals = [](int a, int b) -> bool { return subtreeLabels[a] == subtreeLabels[b]; };
 
-bool equals(int a, int b) {
-    return subtreeLabels[a] == subtreeLabels[b];
-}
-
-void generateMapping(int r1, int r2) {
-    map.resize(n);
-    map[r1] = r2 - n;
+void generate_mapping(int r1, int r2) {
+    mapping.resize(n);
+    mapping[r1] = r2 - n;
     sort(children[r1].begin(), children[r1].end(), compare);
     sort(children[r2].begin(), children[r2].end(), compare);
-    for (int i = 0; i < (int) children[r1].size(); i++) {
+    for (int i = 0; i < children[r1].size(); i++) {
         int u = children[r1][i];
         int v = children[r2][i];
-        generateMapping(u, v);
+        generate_mapping(u, v);
     }
 }
 
-vi findCenter(int offset = 0) {
+vector<int> find_center(int offset = 0) {
     int cnt = n;
-    vi a;
-    vi deg(n);
+    vector<int> a;
+    vector<int> deg(n);
     for (int i = 0; i < n; i++) {
         deg[i] = tree[i + offset].size();
         if (deg[i] <= 1) {
@@ -43,10 +33,10 @@ vi findCenter(int offset = 0) {
         }
     }
     while (cnt > 0) {
-        vi na;
-        for (int i = 0; i < (int) a.size(); i++) {
+        vector<int> na;
+        for (int i = 0; i < a.size(); i++) {
             int u = a[i];
-            for (int j = 0; j < (int) tree[u].size(); j++) {
+            for (int j = 0; j < tree[u].size(); j++) {
                 int v = tree[u][j];
                 if (--deg[v - offset] == 1) {
                     na.push_back(v);
@@ -62,7 +52,7 @@ vi findCenter(int offset = 0) {
 int dfs(int u, int p = -1, int depth = 0) {
     L[depth].push_back(u);
     int h = 0;
-    for (int i = 0; i < (int) tree[u].size(); i++) {
+    for (int i = 0; i < tree[u].size(); i++) {
         int v = tree[u][i];
         if (v == p)
             continue;
@@ -73,10 +63,10 @@ int dfs(int u, int p = -1, int depth = 0) {
     return h + 1;
 }
 
-bool rootedTreeIsomorphism(int r1, int r2) {
-    L.assign(n, vi());
+bool rooted_tree_isomorphism(int r1, int r2) {
+    L.assign(n, vector<int>());
     pred.assign(2 * n, -1);
-    children.assign(2 * n, vi());
+    children.assign(2 * n, vector<int>());
 
     int h1 = dfs(r1);
     int h2 = dfs(r2);
@@ -84,18 +74,18 @@ bool rootedTreeIsomorphism(int r1, int r2) {
         return false;
 
     int h = h1 - 1;
-    vi label(2 * n);
-    subtreeLabels.assign(2 * n, vi());
+    vector<int> label(2 * n);
+    subtreeLabels.assign(2 * n, vector<int>());
 
     for (int i = h - 1; i >= 0; i--) {
-        for (int j = 0; j < (int) L[i + 1].size(); j++) {
+        for (int j = 0; j < L[i + 1].size(); j++) {
             int v = L[i + 1][j];
             subtreeLabels[pred[v]].push_back(label[v]);
         }
 
         sort(L[i].begin(), L[i].end(), compare);
 
-        for (int j = 0, cnt = 0; j < (int) L[i].size(); j++) {
+        for (int j = 0, cnt = 0; j < L[i].size(); j++) {
             if (j && !equals(L[i][j], L[i][j - 1]))
                 ++cnt;
             label[L[i][j]] = cnt;
@@ -105,45 +95,45 @@ bool rootedTreeIsomorphism(int r1, int r2) {
     if (!equals(r1, r2))
         return false;
 
-    generateMapping(r1, r2);
+    generate_mapping(r1, r2);
     return true;
 }
 
 bool treeIsomorphism() {
-    vi c1 = findCenter();
-    vi c2 = findCenter(n);
+    vector<int> c1 = find_center();
+    vector<int> c2 = find_center(n);
     if (c1.size() == c2.size()) {
-        if (rootedTreeIsomorphism(c1[0], c2[0]))
+        if (rooted_tree_isomorphism(c1[0], c2[0]))
             return true;
         else if (c1.size() > 1)
-            return rootedTreeIsomorphism(c1[1], c2[0]);
+            return rooted_tree_isomorphism(c1[1], c2[0]);
     }
     return false;
 }
 
 int main() {
     n = 5;
-    vvi t1(n);
-    t1[0].push_back(1);
-    t1[1].push_back(0);
-    t1[1].push_back(2);
-    t1[2].push_back(1);
-    t1[1].push_back(3);
-    t1[3].push_back(1);
-    t1[0].push_back(4);
-    t1[4].push_back(0);
+    vector<vector<int>> t1(n);
+    t1[0].emplace_back(1);
+    t1[1].emplace_back(0);
+    t1[1].emplace_back(2);
+    t1[2].emplace_back(1);
+    t1[1].emplace_back(3);
+    t1[3].emplace_back(1);
+    t1[0].emplace_back(4);
+    t1[4].emplace_back(0);
 
-    vvi t2(n);
-    t2[0].push_back(1);
-    t2[1].push_back(0);
-    t2[0].push_back(4);
-    t2[4].push_back(0);
-    t2[4].push_back(3);
-    t2[3].push_back(4);
-    t2[4].push_back(2);
-    t2[2].push_back(4);
+    vector<vector<int>> t2(n);
+    t2[0].emplace_back(1);
+    t2[1].emplace_back(0);
+    t2[0].emplace_back(4);
+    t2[4].emplace_back(0);
+    t2[4].emplace_back(3);
+    t2[3].emplace_back(4);
+    t2[4].emplace_back(2);
+    t2[2].emplace_back(4);
 
-    tree.assign(2 * n, vi());
+    tree.assign(2 * n, vector<int>());
     for (int u = 0; u < n; u++) {
         for (int i = 0; i < t1[u].size(); i++) {
             int v = t1[u][i];
@@ -160,5 +150,5 @@ int main() {
 
     if (res)
         for (int i = 0; i < n; i++)
-            cout << map[i] << endl;
+            cout << mapping[i] << endl;
 }
