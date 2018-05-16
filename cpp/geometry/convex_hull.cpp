d20d15a111f7f30e81613c15044f568f9d8208c3
indy256 <indy256@gmail.com>
2018-05-16 03:36:56 +0200

    update

--- cpp/geometry/convex_hull.cpp
+++ cpp/geometry/convex_hull.cpp
@@ -8,7 +8,7 @@ long long cross(const point &a, const point &b, const point &c) {
     return (b.first - a.first) * (c.second - a.second) - (b.second - a.second) * (c.first - a.first);
 }
 
-vector<point> convexHull(vector<point> points) {
+vector<point> convex_hull(vector<point> points) {
     if (points.size() <= 1)
         return points;
     sort(points.begin(), points.end());
@@ -31,13 +31,13 @@ vector<point> convexHull(vector<point> points) {
 
 // usage example
 int main() {
-    vector<point> hull1 = convexHull({{0, 0},
-                                      {3, 0},
-                                      {0, 3},
-                                      {1, 1}});
+    vector<point> hull1 = convex_hull({{0, 0},
+                                       {3, 0},
+                                       {0, 3},
+                                       {1, 1}});
     cout << (3 == hull1.size()) << endl;
 
-    vector<point> hull2 = convexHull({{0, 0},
-                                      {0, 0}});
+    vector<point> hull2 = convex_hull({{0, 0},
+                                       {0, 0}});
     cout << (1 == hull2.size()) << endl;
 }
