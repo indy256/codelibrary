622d9d6cc1e1cefbb82613bd84a73c2202798b9d
anavumenka <andrei.navumenka@zalando.de>
2018-05-16 13:28:12 +0200

    update

---
+++ cpp/structures/segment_tree_interval_add_max.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 200000;
+int tmax[4 * maxn];
+int tadd[4 * maxn]; // tadd[i] applies to tmax[i], tadd[2*i+1] and tadd[2*i+2]
+
+void push(int root) {
+    tmax[root] += tadd[root];
+    tadd[2 * root + 1] += tadd[root];
+    tadd[2 * root + 2] += tadd[root];
+    tadd[root] = 0;
+}
+
+int max(int from, int to, int root = 0, int left = 0, int right = maxn - 1) {
+    if (from > right || left > to)
+        return INT_MIN;
+    if (from <= left && right <= to) {
+        return tmax[root] + tadd[root];
+    }
+    push(root);
+    int mid = (left + right) >> 1;
+    int res = INT_MIN;
+    if (from <= mid)
+        res = std::max(res, max(from, to, 2 * root + 1, left, mid));
+    if (to > mid)
+        res = std::max(res, max(from, to, 2 * root + 2, mid + 1, right));
+    return res;
+}
+
+void add(int from, int to, int delta, int root = 0, int left = 0, int right = maxn - 1) {
+    if (from > right || left > to)
+        return;
+    if (from <= left && right <= to) {
+        tadd[root] += delta;
+        return;
+    }
+    push(root); // this push may be omitted for add, but is necessary for other operations such as set
+    int mid = (left + right) >> 1;
+    if (from <= mid)
+        add(from, to, delta, 2 * root + 1, left, mid);
+    if (to > mid)
+        add(from, to, delta, 2 * root + 2, mid + 1, right);
+    tmax[root] = std::max(tmax[2 * root + 1] + tadd[2 * root + 1], tmax[2 * root + 2] + tadd[2 * root + 2]);
+}
+
+// usage example
+int main() {
+    add(0, 9, 1);
+    add(2, 4, 2);
+    add(3, 5, 3);
+
+    cout << (6 == max(0, 9)) << endl;
+    cout << (6 == tmax[0] + tadd[0]) << endl;
+    cout << (1 == max(0, 0)) << endl;
+}
