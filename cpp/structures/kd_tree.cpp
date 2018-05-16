e065c9ca3d17502ee36f3ea3c6e114037eded9bd
anavumenka <andrei.navumenka@zalando.de>
2018-05-16 10:34:10 +0200

    update

--- cpp/structures/kd_tree.cpp
+++ cpp/structures/kd_tree.cpp
@@ -3,20 +3,11 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-typedef vector<pii> vpii;
 
 const int maxn = 100000;
 int tx[maxn];
 int ty[maxn];
 bool divX[maxn];
-
-bool cmpX(const pii &a, const pii &b) {
-    return a.first < b.first;
-}
-
-bool cmpY(const pii &a, const pii &b) {
-    return a.second < b.second;
-}
 
 void buildTree(int left, int right, pii points[]) {
     if (left >= right)
@@ -29,12 +20,14 @@ void buildTree(int left, int right, pii points[]) {
     int miny = INT_MAX;
     int maxy = INT_MIN;
     for (int i = left; i < right; i++) {
-        checkmin(minx, points[i].first);
-        checkmax(maxx, points[i].first);
-        checkmin(miny, points[i].second);
-        checkmax(maxy, points[i].second);
+        minx = min(minx, points[i].first);
+        maxx = max(maxx, points[i].first);
+        miny = min(miny, points[i].second);
+        maxy = max(maxy, points[i].second);
     }
     divX[mid] = (maxx - minx) >= (maxy - miny);
+    auto cmpX = [](const pii &a, const pii &b) { return a.first < b.first; };
+    auto cmpY = [](const pii &a, const pii &b) { return a.second < b.second; };
     nth_element(points + left, points + mid, points + right, divX[mid] ? cmpX : cmpY);
 
     tx[mid] = points[mid].first;
@@ -85,15 +78,15 @@ int findNearestNeighbour(int n, int x, int y) {
 
 // usage example
 int main() {
-    vpii p;
-    p.push_back(make_pair(0, 2));
-    p.push_back(make_pair(0, 3));
-    p.push_back(make_pair(-1, 0));
+    vector<pii> p;
+    p.emplace_back(0, 2);
+    p.emplace_back(0, 3);
+    p.emplace_back(-1, 0);
 
     p.resize(unique(p.begin(), p.end()) - p.begin());
 
     int n = p.size();
-    buildTree(1, 0, n - 1, &(vpii(p)[0]));
+    buildTree(0, n - 1, &p[0]);
     int res = findNearestNeighbour(n, 0, 0);
 
     cout << p[res].first << " " << p[res].second << endl;
