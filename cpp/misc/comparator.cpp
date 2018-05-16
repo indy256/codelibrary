818f7716a8ee2b0e927ec5ea804f99836272ca63
anavumenka <andrei.navumenka@zalando.de>
2018-05-16 14:25:07 +0200

    update

--- cpp/misc/comparator.cpp
+++ cpp/misc/comparator.cpp
@@ -1,10 +1,10 @@
-#include <algorithm>
-#include <set>
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 struct item {
     int x, y;
+
     bool operator<(const item &o) const {
         return x < o.x || x == o.x && y < o.y;
     }
@@ -20,18 +20,19 @@ bool cmp(const item &a, const item &b) {
     return a.x < b.x || a.x == b.x && a.y < b.y;
 }
 
+auto cmpl = [](const item &a, const item &b) { return a.x < b.x || a.x == b.x && a.y < b.y; };
+
 int main() {
-    item a[] = { { 2, 3 }, { 1, 2 } };
-    //typedef set<item, bool(*)(const item&, const item&)> myset;
-    //myset s(a, a + 2, cmp);
-    //typedef set<item, item_cmp> myset;
-    typedef set<item> myset;
-    myset s(a, a + 2);
-    for (myset::iterator it = s.begin(); it != s.end(); it++) {
-        cout << it->x << " " << it->y << endl;
+    item a[] = {{2, 3},
+                {1, 2}};
+
+    set<item, bool (*)(const item &, const item &)> s(a, a + 2, cmp);
+    for (const item &it : s) {
+        cout << it.x << " " << it.y << endl;
     }
 
-    sort(a, a + 2, cmp);
     sort(a, a + 2, item_cmp());
+    sort(a, a + 2, cmp);
+    sort(a, a + 2, cmpl);
     cout << a[0].x << " " << a[0].y << endl;
 }
