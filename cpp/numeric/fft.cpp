b46864d585ce59f6b36e55ddcf2eeef308cb3e16
anavumenka <andrei.navumenka@zalando.de>
2018-05-31 10:20:37 +0200

    update

--- cpp/numeric/fft.cpp
+++ cpp/numeric/fft.cpp
@@ -10,31 +10,27 @@ typedef complex<double> cpx;
 // precondition: a.size() is a power of 2
 void fft(vector<cpx> &a, bool inverse) {
     size_t n = a.size();
-    for (int i = 1, j = 0; i < n; ++i) {
-        int bit = n >> 1;
-        for (; j >= bit; bit >>= 1)
-            j -= bit;
-        j += bit;
-        if (i < j)
-            swap(a[i], a[j]);
+    if (n == 1) return;
+
+    vector<cpx> a0(n / 2);
+    vector<cpx> a1(n / 2);
+    for (int i = 0; i < n / 2; i++) {
+        a0[i] = a[2 * i];
+        a1[i] = a[2 * i + 1];
     }
-    for (int len = 2; len <= n; len *= 2) {
-        double angle = 2 * M_PI / len * (inverse ? -1 : 1);
-        cpx wlen(cos(angle), sin(angle));
-        for (int i = 0; i < n; i += len) {
-            cpx w(1);
-            for (int j = 0; j < len / 2; j++) {
-                cpx u = a[i + j];
-                cpx v = a[i + j + len / 2] * w;
-                a[i + j] = u + v;
-                a[i + j + len / 2] = u - v;
-                w *= wlen;
-            }
-        }
+    fft(a0, inverse);
+    fft(a1, inverse);
+
+    double ang = 2 * M_PI / n * (inverse ? -1 : 1);
+    cpx w(1);
+    cpx wn(cos(ang), sin(ang));
+    for (int i = 0; i < n / 2; ++i) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n / 2] = a0[i] - w * a1[i];
+        if (inverse)
+            a[i] /= 2, a[i + n / 2] /= 2;
+        w *= wn;
     }
-    if (inverse)
-        for (int i = 0; i < n; i++)
-            a[i] /= n;
 }
 
 vector<int> multiply(const vector<int> &a, const vector<int> &b) {
