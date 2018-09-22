ea49af5cc77b97368f1c4e8fc831116c78c4476f
indy256 <indy256@gmail.com>
2018-09-22 15:31:07 +0200

    update

--- java/combinatorics/NumberIterator.java
+++ java/combinatorics/NumberIterator.java
@@ -1,8 +1,6 @@
 package combinatorics;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.stream.IntStream;
 
