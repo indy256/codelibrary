97e5b15bbb7264c113bbb0e4f918ee8ee41ff76f
indy256 <indy256@gmail.com>
2018-12-27 11:56:02 +0100

    update

--- cpp/misc/comparator.cpp
+++ cpp/misc/comparator.cpp
@@ -10,17 +10,22 @@ int main() {
     item a[] = {{2, 3},
                 {1, 2}};
 
-    auto comparator = [](auto &a, auto &b) { return a.x < b.x || a.x == b.x && a.y < b.y; };
+    auto cmp = [](auto &a, auto &b) { return a.x < b.x || a.x == b.x && a.y < b.y; };
 
-    sort(a, a + 2, comparator);
+    sort(a, a + 2, cmp);
+    set<item, decltype(cmp)> s(a, a + 2, cmp);
+    map<item, int, decltype(cmp)> m({{item{2, 3}, 1}}, cmp);
+    priority_queue<item, vector<item>, decltype(cmp)> q(a, a + 2, cmp);
 
-    set<item, decltype(comparator)> s(a, a + 2, comparator);
-    for (const item &it : s) {
+    for (auto &it : s) {
         cout << it.x << " " << it.y << endl;
     }
 
-    priority_queue<item, vector<item>, decltype(comparator)> q(a, a + 2, comparator);
-    while(!q.empty()) {
+    for (auto &entry : m) {
+        cout << entry.first.x << " " << entry.first.y << " " << entry.second << endl;
+    }
+
+    while (!q.empty()) {
         auto item = q.top();
         q.pop();
         cout << item.x << " " << item.y << endl;
