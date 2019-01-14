2adf0f53c2b475d27b838f44fb7a6b2c40622a92
Andrei Navumenka <andrei.navumenka@zalando.de>
2019-01-14 13:43:50 +0100

    update

---
+++ cpp/graphs/tree_centers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// returns vertex that has all its subtrees sizes <= n/2
+int find_tree_centroid(vector<vector<int>> &tree, int u, int p) {
+    int n = tree.size();
+    int cnt = 1;
+    bool goodCenter = true;
+    for (int v : tree[u]) {
+        if (v == p) continue;
+        int res = find_tree_centroid(tree, v, u);
+        if (res >= 0)
+            return res;
+        int size = -res;
+        goodCenter &= size <= n / 2;
+        cnt += size;
+    }
+    goodCenter &= n - cnt <= n / 2;
+    return goodCenter ? u : -cnt;
+}
+
+// usage example
+int main() {
+}
---
+++ cpp/structures/centroid_decomposition.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void calc_sizes(vector<vector<int>> &tree, vector<int> &size, vector<bool> &deleted, int u, int p) {
+    size[u] = 1;
+    for (int v : tree[u]) {
+        if (v == p || deleted[v]) continue;
+        calc_sizes(tree, size, deleted, v, u);
+        size[u] += size[v];
+    }
+}
+
+int find_tree_centroid(vector<vector<int>> &tree, vector<int> &size, vector<bool> &deleted, int u, int p,
+                       int vertices) {
+    for (int v : tree[u]) {
+        if (v == p || deleted[v]) continue;
+        if (size[v] > vertices / 2) {
+            return find_tree_centroid(tree, size, deleted, v, u, vertices);
+        }
+    }
+    return u;
+}
+
+void dfs(vector<vector<int>> &tree, vector<bool> &deleted, int u, int p) {
+    for (int v: tree[u]) {
+        if (v == p || deleted[v])continue;
+        dfs(tree, deleted, v, u);
+    }
+}
+
+void decompose(vector<vector<int>> &tree, vector<int> &size, vector<bool> &deleted, int u, int total) {
+    calc_sizes(tree, size, deleted, u, -1);
+    int centroid = find_tree_centroid(tree, size, deleted, u, -1, total);
+    deleted[centroid] = true;
+
+    // process centroid vertex here
+    // cout << centroid << endl;
+
+    // for (int v: tree[centroid]) {
+    //    if (deleted[v])continue;
+    //    dfs(tree, deleted, v, centroid);
+    // }
+
+    for (int v : tree[centroid]) {
+        if (deleted[v]) continue;
+        decompose(tree, size, deleted, v, size[v]);
+    }
+}
+
+// usage example
+int main() {
+    vector<vector<int>> tree = {{3},
+                                {3},
+                                {3},
+                                {0, 1, 2}};
+
+    int n = tree.size();
+    vector<int> size(n);
+    vector<bool> deleted(n);
+    decompose(tree, size, deleted, 0, n);
+}
