194105e9022df7f19f0d6c26281d91a4a4a7d3b2
indy256 <indy256@gmail.com>
2019-01-18 01:36:36 +0100

    update

--- cpp/structures/heavy_light_decomposition.cpp
+++ cpp/structures/heavy_light_decomposition.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 class SegmentTree {
-    vector<int> value;
-    vector<int> delta; // // delta[i] affects value[i], delta[2*i+1] and delta[2*i+2]
 public:
-    explicit SegmentTree(int n) : value(2 * n), delta(2 * n, neutral_delta) {
-        fill(value.begin(), value.begin() + n, neutral_value);
+    explicit SegmentTree(int n) : value(2 * n), delta(2 * n, neutral_delta()) {
+        fill(value.begin(), value.begin() + n, neutral_value());
         for (int i = 2 * n - 1; i > 1; i -= 2) {
             value[i >> 1] = query_operation(value[i], value[i ^ 1]);
         }
@@ -18,7 +16,7 @@ public:
         to += value.size() >> 1;
         push_delta(from);
         push_delta(to);
-        int res = neutral_value;
+        int res = neutral_value();
         bool found = false;
         for (int len = 1; from <= to; from = (from + 1) >> 1, to = (to - 1) >> 1, len <<= 1) {
             if ((from & 1) != 0) {
@@ -58,19 +56,30 @@ public:
         }
     }
 
+    vector<int> value;
+    vector<int> delta; // delta[i] affects value[i], delta[2*i+1] and delta[2*i+2]
+
+    int neutral_delta() {
+        return 0;
+    }
+
+    int neutral_value() {
+        return 0;
+    }
+
     // Modify the following 5 methods to implement your custom operations on the tree.
     // This example implements Add/Sum operations. Operations like Add/Max, Set/Max can also be implemented.
-    constexpr static auto update_operation = [](int x, int y) -> int { return x + y; };
+    int update_operation(int x, int y) {
+        return x + y;
+    }
 
     // query (or combine) operation
-    constexpr static auto query_operation = [](int leftValue, int rightValue) -> int { return leftValue + rightValue; };
+    int query_operation(int leftValue, int rightValue) {
+        return leftValue + rightValue;
+    }
 
-    const int neutral_delta = 0;
-    const int neutral_value = 0;
-
-private:
     int delta_effect_on_segment(int delta, int segmentLength) {
-        if (delta == neutral_delta) return neutral_delta;
+        if (delta == neutral_delta()) return neutral_delta();
         // Here you must write a fast equivalent of following slow code:
         // int result = delta;
         // for (int i = 1; i < segmentLength; i++) result = queryOperation(result, delta);
@@ -79,13 +88,13 @@ private:
     }
 
     int join_value_with_delta(int value, int delta) {
-        if (delta == neutral_delta) return value;
+        if (delta == neutral_delta()) return value;
         return update_operation(value, delta);
     }
 
     int join_deltas(int delta1, int delta2) {
-        if (delta1 == neutral_delta) return delta2;
-        if (delta2 == neutral_delta) return delta1;
+        if (delta1 == neutral_delta()) return delta2;
+        if (delta2 == neutral_delta()) return delta1;
         return update_operation(delta1, delta2);
     }
 
@@ -98,7 +107,7 @@ private:
             value[x >> 1] = join_node_value_with_delta(x >> 1, 1 << (d + 1));
             delta[x] = join_deltas(delta[x], delta[x >> 1]);
             delta[x ^ 1] = join_deltas(delta[x ^ 1], delta[x >> 1]);
-            delta[x >> 1] = neutral_delta;
+            delta[x >> 1] = neutral_delta();
         }
     }
 
@@ -160,7 +169,7 @@ public:
 
     int query(int u, int v) {
         int res = getNeutralValue();
-        auto op = [this, &res](int a, int b) { res = SegmentTree::query_operation(res, segment_tree.query(a, b)); };
+        auto op = [this, &res](int a, int b) { res = segment_tree.query_operation(res, segment_tree.query(a, b)); };
         process_path(u, v, op);
         return res;
     }
@@ -195,10 +204,10 @@ int main() {
     HeavyLight hl_v(tree, true);
     hl_v.modify(3, 2, 1);
     hl_v.modify(1, 0, -1);
-    cout << (1 == hl_v.query(4, 2)) << endl;
+    cout << (hl_v.query(4, 2)) << endl;
 
     HeavyLight hl_e(tree, false);
     hl_e.modify(3, 2, 1);
     hl_e.modify(1, 0, -1);
-    cout << (1 == hl_e.query(4, 2)) << endl;
+    cout << (hl_e.query(4, 2)) << endl;
 }
--- cpp/structures/segment_tree_fast.cpp
+++ cpp/structures/segment_tree_fast.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 class SegmentTreeFast {
-    vector<int> value;
-    vector<int> delta; // // delta[i] affects value[i], delta[2*i+1] and delta[2*i+2]
 public:
-    explicit SegmentTreeFast(int n) : value(2 * n), delta(2 * n, neutral_delta) {
-        fill(value.begin(), value.begin() + n, neutral_value);
+    explicit SegmentTreeFast(int n) : value(2 * n), delta(2 * n, neutral_delta()) {
+        fill(value.begin(), value.begin() + n, neutral_value());
         for (int i = 2 * n - 1; i > 1; i -= 2) {
             value[i >> 1] = query_operation(value[i], value[i ^ 1]);
         }
@@ -18,7 +16,7 @@ public:
         to += value.size() >> 1;
         push_delta(from);
         push_delta(to);
-        int res = neutral_value;
+        int res = neutral_value();
         bool found = false;
         for (int len = 1; from <= to; from = (from + 1) >> 1, to = (to - 1) >> 1, len <<= 1) {
             if ((from & 1) != 0) {
@@ -58,19 +56,30 @@ public:
         }
     }
 
-private:
+    vector<int> value;
+    vector<int> delta; // delta[i] affects value[i], delta[2*i+1] and delta[2*i+2]
+
+    int neutral_delta() {
+        return 0;
+    }
+
+    int neutral_value() {
+        return 0;
+    }
+
     // Modify the following 5 methods to implement your custom operations on the tree.
     // This example implements Add/Sum operations. Operations like Add/Max, Set/Max can also be implemented.
-    constexpr static auto update_operation = [](int x, int y) -> int { return x + y; };
+    int update_operation(int x, int y) {
+        return x + y;
+    }
 
     // query (or combine) operation
-    constexpr static auto query_operation = [](int leftValue, int rightValue) -> int { return leftValue + rightValue; };
-
-    const int neutral_delta = 0;
-    const int neutral_value = 0;
+    int query_operation(int leftValue, int rightValue) {
+        return leftValue + rightValue;
+    }
 
     int delta_effect_on_segment(int delta, int segmentLength) {
-        if (delta == neutral_delta) return neutral_delta;
+        if (delta == neutral_delta()) return neutral_delta();
         // Here you must write a fast equivalent of following slow code:
         // int result = delta;
         // for (int i = 1; i < segmentLength; i++) result = queryOperation(result, delta);
@@ -79,13 +88,13 @@ private:
     }
 
     int join_value_with_delta(int value, int delta) {
-        if (delta == neutral_delta) return value;
+        if (delta == neutral_delta()) return value;
         return update_operation(value, delta);
     }
 
     int join_deltas(int delta1, int delta2) {
-        if (delta1 == neutral_delta) return delta2;
-        if (delta2 == neutral_delta) return delta1;
+        if (delta1 == neutral_delta()) return delta2;
+        if (delta2 == neutral_delta()) return delta1;
         return update_operation(delta1, delta2);
     }
 
@@ -98,7 +107,7 @@ private:
             value[x >> 1] = join_node_value_with_delta(x >> 1, 1 << (d + 1));
             delta[x] = join_deltas(delta[x], delta[x >> 1]);
             delta[x ^ 1] = join_deltas(delta[x ^ 1], delta[x >> 1]);
-            delta[x >> 1] = neutral_delta;
+            delta[x >> 1] = neutral_delta();
         }
     }
 
