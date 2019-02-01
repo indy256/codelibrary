d747d1b443103d45b5257ea3fcbf0e51d26f7ef7
Andrei Navumenka <andrei.navumenka@zalando.de>
2019-02-01 09:22:44 +0100

    update

--- cpp/numbertheory/primitive_root.cpp
+++ cpp/numbertheory/primitive_root.cpp
@@ -15,11 +15,23 @@ int powmod(int x, int n, int mod) {
     return res;
 }
 
+int totient_function(int n) {
+    int res = n;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            res -= res / i;
+        }
+    if (n > 1)
+        res -= res / n;
+    return res;
+}
+
 // returns g such that g^i runs through all numbers from 1 to p-1 modulo p
-// p - prime
 int generator(int p) {
     vector<int> factors;
-    int phi = p - 1;
+    int phi = totient_function(p);
     int n = phi;
     for (int i = 2; i * i <= n; ++i)
         if (n % i == 0) {
@@ -30,6 +42,7 @@ int generator(int p) {
     if (n > 1)
         factors.push_back(n);
     for (int res = 2; res <= p; ++res) {
+        if (gcd(res, p) != 1) continue;
         bool ok = true;
         for (size_t i = 0; i < factors.size() && ok; ++i)
             ok &= powmod(res, phi / factors[i], p) != 1;
