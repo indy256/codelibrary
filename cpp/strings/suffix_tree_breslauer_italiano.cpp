24a898876c3c17cbf87c0f09f70acdd62facb1a2
Andrei Navumenka <andrei.navumenka@zalando.de>
2019-02-15 18:07:56 +0100

    update

--- cpp/strings/suffix_tree_breslauer_italiano.cpp
+++ cpp/strings/suffix_tree_breslauer_italiano.cpp
@@ -17,31 +17,36 @@ void attach(int child, int parent, char c, int child_len) {
 }
 
 void extend(char c) {
-    int p;
+    int v;
     int i = s.size();
     int old = sz - 1;
-    for (p = old; !link[p].count(c); p = par[p])
-        i -= len[p];
-    int v = link[p][c];
-    if (to[v].count(s[i])) {
-        int u = to[v][c];
-        for (pos[sz] = pos[u] - len[u]; s[pos[sz]] == s[i]; pos[sz] += len[p], i += len[p])
-            p = to[p][s[i]];
-        attach(sz, v, s[pos[u] - len[u]], len[u] - (pos[u] - pos[sz]));
+    for (v = old; !link[v].count(c); v = par[v])
+        i -= len[v];
+    int w = link[v][c];
+    if (to[w].count(s[i])) {
+        int u = to[w][c];
+        for (pos[sz] = pos[u] - len[u]; s[pos[sz]] == s[i]; pos[sz] += len[v], i += len[v])
+            v = to[v][s[i]];
+        attach(sz, w, s[pos[u] - len[u]], len[u] - (pos[u] - pos[sz]));
         attach(u, sz, s[pos[sz]], pos[u] - pos[sz]);
-        v = link[p][c] = sz++;
+        w = link[v][c] = sz++;
     }
     link[old][c] = sz;
-    attach(sz, v, s[i], s.size() - i);
+    attach(sz, w, s[i], s.size() - i);
     pos[sz++] = s.size();
 }
 
-int main() {
+void init_tree() {
     len[1] = 1;
     pos[1] = 0;
     par[1] = 0;
     for (int c = 0; c <= 255; c++)
         to[0][c] = link[0][c] = 1;
+}
+
+int main() {
+    init_tree();
+
     s = "aabababbbbadcasdf#";
     for (int i = s.size() - 1; i >= 0; i--)
         extend(s[i]);
