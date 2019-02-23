1bc932b9159077e5fe8c6e2a6db6e9a6ae890042
Andrei Navumenka <andrei.navumenka@zalando.de>
2019-02-23 21:12:56 +0100

    update

--- cpp/structures/disjoint_sets_ranked.cpp
+++ cpp/structures/disjoint_sets_ranked.cpp
@@ -4,26 +4,30 @@ using namespace std;
 
 // https://cp-algorithms.com/data_structures/disjoint_set_union.html
 
-vector<int> create_sets(int n) {
+tuple<vector<int>, vector<int>> create_sets(int n) {
     vector<int> res(n);
     iota(res.begin(), res.end(), 0);
-    return res;
+    vector<int> rank(n);
+    return {res, rank};
 }
 
 int root(vector<int> &p, int x) {
     return x == p[x] ? x : (p[x] = root(p, p[x]));
 }
 
-void unite(vector<int> &p, int a, int b) {
+void unite(vector<int> &p, vector<int> &rank, int a, int b) {
     a = root(p, a);
     b = root(p, b);
+    if (a == b) return;
+    if (rank[a] < rank[b]) swap(a, b);
+    if (rank[a] == rank[b]) ++rank[a];
     p[b] = a;
 }
 
 // usage example
 int main() {
-    vector<int> p = create_sets(3);
-    unite(p, 0, 2);
+    auto[p, rank] = create_sets(3);
+    unite(p, rank, 0, 2);
     cout << (0 == root(p, 0)) << endl;
     cout << (1 == root(p, 1)) << endl;
     cout << (0 == root(p, 2)) << endl;
