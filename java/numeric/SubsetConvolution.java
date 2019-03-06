052829801313aa54f66c0c84999bf3bee76a0a97
Andrei Navumenka <andrei.navumenka@zalando.de>
2019-03-06 18:05:55 +0100

    update

--- java/numeric/SubsetConvolution.java
+++ java/numeric/SubsetConvolution.java
@@ -4,36 +4,36 @@ import java.util.Arrays;
 
 public class SubsetConvolution {
 
-    // calculates res[i] = sum(a[j]*b[i^j] | j&i==j) in O(2^log(n) * log^2(n))
+    // calculates c[k] = sum(a[i]*b[j] | i|j==k, i&j==0) in O(2^log(n) * log^2(n))
     public static int[] subsetConvolution(int[] a, int[] b) {
         int n = a.length;
         int logn = Integer.bitCount(n - 1) + 1;
-        int[][] moba = new int[logn][n];
-        int[][] mobb = new int[logn][n];
+        int[][] ta = new int[logn][n];
+        int[][] tb = new int[logn][n];
         for (int i = 0; i < n; ++i) {
-            moba[Integer.bitCount(i)][i] = a[i];
-            mobb[Integer.bitCount(i)][i] = b[i];
+            ta[Integer.bitCount(i)][i] = a[i];
+            tb[Integer.bitCount(i)][i] = b[i];
         }
 
         for (int i = 0; i < logn; ++i) {
             for (int j = 0; j < logn; ++j) {
                 for (int k = 0; k < n; ++k) {
                     if (((k >> j) & 1) != 0) {
-                        moba[i][k] += moba[i][k ^ (1 << j)];
-                        mobb[i][k] += mobb[i][k ^ (1 << j)];
+                        ta[i][k] += ta[i][k ^ (1 << j)];
+                        tb[i][k] += tb[i][k ^ (1 << j)];
                     }
                 }
             }
         }
 
-        int[][] mobc = new int[logn][n];
+        int[][] tc = new int[logn][n];
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < logn; ++j) {
                 int res = 0;
                 for (int k = 0; k <= j; ++k) {
-                    res += moba[k][i] * mobb[j - k][i];
+                    res += ta[k][i] * tb[j - k][i];
                 }
-                mobc[j][i] = res;
+                tc[j][i] = res;
             }
         }
 
@@ -41,7 +41,7 @@ public class SubsetConvolution {
             for (int j = 0; j < logn; ++j) {
                 for (int k = 0; k < n; ++k) {
                     if (((k >> j) & 1) != 0) {
-                        mobc[i][k] -= mobc[i][k ^ (1 << j)];
+                        tc[i][k] -= tc[i][k ^ (1 << j)];
                     }
                 }
             }
@@ -49,7 +49,7 @@ public class SubsetConvolution {
 
         int[] result = new int[n];
         for (int i = 0; i < n; ++i) {
-            result[i] = mobc[Integer.bitCount(i)][i];
+            result[i] = tc[Integer.bitCount(i)][i];
         }
         return result;
     }
---
+++ java/numeric/WalshHadamarTransform.java
@@ -0,0 +1,61 @@
+package numeric;
+
+import java.util.Arrays;
+
+import static numeric.WalshHadamarTransform.Operation.XOR;
+
+public class WalshHadamarTransform {
+
+    enum Operation {XOR, OR, AND}
+
+    // calculates c[k] = sum(a[i]*b[j] | i op j == k), where op = XOR | OR | AND
+    // complexity: O(n*log(n))
+    public static int[] convolution(int[] a, int[] b, Operation op) {
+        transform(a, op, false);
+        transform(b, op, false);
+        for (int i = 0; i < a.length; i++) {
+            a[i] *= b[i];
+        }
+        transform(a, op, true);
+        return a;
+    }
+
+    static void transform(int[] a, Operation op, boolean inverse) {
+        int n = a.length;
+
+        for (int step = 1; step < n; step *= 2) {
+            for (int i = 0; i < n; i += 2 * step) {
+                for (int j = i; j < i + step; ++j) {
+                    int u = a[j];
+                    int v = a[j + step];
+                    switch (op) {
+                        case XOR:
+                            a[j] = u + v;
+                            a[j + step] = u - v;
+                            break;
+                        case OR:
+                            a[j] = inverse ? v : u + v;
+                            a[j + step] = inverse ? u - v : u;
+                            break;
+                        case AND:
+                            a[j] = inverse ? v - u : v;
+                            a[j + step] = inverse ? u : v + u;
+                            break;
+                    }
+                }
+            }
+        }
+        if (op == XOR && inverse) {
+            for (int i = 0; i < n; i++) {
+                a[i] /= n;
+            }
+        }
+    }
+
+    // Usage example
+    public static void main(String[] args) {
+        int[] a = {3, 2, 1, 5};
+        int[] b = {6, 3, 4, 8};
+        System.out.println(Arrays.toString(convolution(a, b, XOR)));
+    }
+}
