c305f138ec9af76b8d5766e192418acc15ab1150
indy256 <indy256@gmail.com>
2019-06-13 02:24:30 +0300

    update

--- java/misc/TernarySearch.java
+++ java/misc/TernarySearch.java
@@ -4,23 +4,24 @@ import java.util.Random;
 import java.util.function.*;
 
 // https://en.wikipedia.org/wiki/Ternary_search
+// Finds the smallest i in [a, b] that maximizes f(i), assuming that f(a) < ... < f(i) ≥ ··· ≥ f(b)
 public class TernarySearch {
 
-    // finds maximum of strictly increasing and then strictly decreasing function
-    public static double ternarySearch(DoubleUnaryOperator f, double lo, double hi) {
-        for (int step = 0; step < 1000; step++) {
-            double m1 = lo + (hi - lo) / 3;
-            double m2 = hi - (hi - lo) / 3;
-            if (f.applyAsDouble(m1) < f.applyAsDouble(m2))
-                lo = m1;
-            else
-                hi = m2;
+    public static int ternarySearch(IntUnaryOperator f, int fromInclusive, int toInclusive) {
+        int lo = fromInclusive - 1;
+        int hi = toInclusive;
+        while (hi - lo > 1) {
+            int mid = (lo + hi) >>> 1;
+            if (f.applyAsInt(mid) < f.applyAsInt(mid + 1)) {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
         }
-        return (lo + hi) / 2;
+        return hi;
     }
 
-    // finds maximum of strictly increasing and then strictly decreasing function
-    public static int ternarySearch(IntUnaryOperator f, int fromInclusive, int toInclusive) {
+    public static int ternarySearch2(IntUnaryOperator f, int fromInclusive, int toInclusive) {
         int lo = fromInclusive;
         int hi = toInclusive;
         while (hi > lo + 2) {
@@ -38,26 +39,24 @@ public class TernarySearch {
         return res;
     }
 
-    public static int ternarySearch2(IntUnaryOperator f, int fromInclusive, int toInclusive) {
-        int lo = fromInclusive - 1;
-        int hi = toInclusive;
-        while (hi - lo > 1) {
-            int mid = (lo + hi) >>> 1;
-            if (f.applyAsInt(mid) < f.applyAsInt(mid + 1)) {
-                lo = mid;
-            } else {
-                hi = mid;
-            }
+    public static double ternarySearchDouble(DoubleUnaryOperator f, double lo, double hi) {
+        for (int step = 0; step < 1000; step++) {
+            double m1 = lo + (hi - lo) / 3;
+            double m2 = hi - (hi - lo) / 3;
+            if (f.applyAsDouble(m1) < f.applyAsDouble(m2))
+                lo = m1;
+            else
+                hi = m2;
         }
-        return hi;
+        return (lo + hi) / 2;
     }
 
     // random tests
     public static void main(String[] args) {
-        System.out.println(ternarySearch((DoubleUnaryOperator) x -> -(x - 2) * (x - 2), -10, 10));
+        System.out.println(ternarySearchDouble(x -> -(x - 2) * (x - 2), -10, 10));
 
         Random rnd = new Random(1);
-        for (int step = 0; step < 1000; step++) {
+        for (int step = 0; step < 10_000; step++) {
             int n = rnd.nextInt(20) + 1;
             int p = rnd.nextInt(n);
             int[] a = new int[n];
@@ -66,10 +65,10 @@ public class TernarySearch {
             for (int i = p - 1; i >= 0; i--)
                 a[i] = a[i + 1] - rnd.nextInt(range) - 1;
             for (int i = p + 1; i < n; i++)
-                a[i] = a[i - 1] - rnd.nextInt(range) - 1;
-            int res1 = ternarySearch((IntUnaryOperator) i -> a[i], 0, a.length - 1);
+                a[i] = a[i - 1] - rnd.nextInt(range);
+            int res = ternarySearch(i -> a[i], 0, a.length - 1);
             int res2 = ternarySearch2(i -> a[i], 0, a.length - 1);
-            if (p != res1 || p != res2)
+            if (p != res || p != res2)
                 throw new RuntimeException();
         }
     }
