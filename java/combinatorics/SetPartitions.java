1bef1d6fa66f3a336e3667c9bfd47f42d562ba47
indy256 <indy256@gmail.com>
2019-06-29 14:23:56 +0300

    update

--- java/combinatorics/SetPartitions.java
+++ java/combinatorics/SetPartitions.java
@@ -1,33 +1,41 @@
 package combinatorics;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class SetPartitions {
 
-    public static int[][] generateSetPartitions(int n) {
-        if (n == 1)
-            return new int[][]{{0}};
-        int[][] p = generateSetPartitions(n - 1);
-        int[] m = new int[p.length];
-        int len = 0;
-        for (int i = 0; i < p.length; i++) {
-            for (int v : p[i])
-                m[i] = Math.max(m[i], 1 + v);
-            len += m[i] + 1;
+    public static boolean nextRestrictedGrowthString(int[] a) {
+        int n = a.length;
+        int[] b = new int[n];
+        for (int i = 0; i + 1 < n; i++) {
+            b[i + 1] = Math.max(a[i] + 1, b[i]);
         }
-        int[][] res = new int[len][n];
-        for (int i = 0, pos = 0; i < p.length; i++) {
-            for (int j = 0; j <= m[i]; j++, pos++) {
-                System.arraycopy(p[i], 0, res[pos], 0, n - 1);
-                res[pos][n - 1] = (i % 2 == 1 ? j + 1 : m[i] - j + 1) % (m[i] + 1);
-            }
+        int i = n - 1;
+        while (a[i] == b[i]) {
+            if (--i < 0) return false;
         }
-        return res;
+        ++a[i];
+        Arrays.fill(a, i + 1, n, 0);
+        return true;
+    }
+
+    public static int[][] toSets(int[] a) {
+        List<Integer>[] sets = Stream.generate(ArrayList::new).limit(a.length).toArray(List[]::new);
+        for (int i = 0; i < a.length; i++) {
+            sets[a[i]].add(i);
+        }
+        return Arrays.stream(sets)
+                .filter(s -> !s.isEmpty())
+                .map(s -> s.stream().mapToInt(Integer::intValue).toArray())
+                .toArray(int[][]::new);
     }
 
     // Usage example
     public static void main(String[] args) {
-        for (int i = 1; i < 5; i++)
-            System.out.println(Arrays.deepToString(generateSetPartitions(i)));
+        int[] a = new int[3];
+        do {
+            System.out.println(Arrays.deepToString(toSets(a)));
+        } while (nextRestrictedGrowthString(a));
     }
 }
