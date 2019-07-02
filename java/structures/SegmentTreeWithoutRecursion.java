c97d292347ae708c74325e3816ebe45c6c959879
indy256 <indy256@gmail.com>
2019-07-02 23:59:33 +0300

    update

--- java/structures/SegmentTreeWithoutRecursion.java
+++ java/structures/SegmentTreeWithoutRecursion.java
@@ -1,6 +1,6 @@
 package structures;
 
-public class SegmentTreeNonRec {
+public class SegmentTreeWithoutRecursion {
 
     public static int get(int[] t, int i) {
         return t[i + t.length / 2];
