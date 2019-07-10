b373629d353deab5de73e912e8d900ba16bc6ead
indy256 <indy256@gmail.com>
2019-07-11 02:17:38 +0300

    update

--- cpp/linearalgebra/gauss.cpp
+++ cpp/linearalgebra/gauss.cpp
@@ -29,7 +29,7 @@ vector<T> gauss(vector<vector<T>> A, vector<T> b) {
     int n = A.size();
     for (int i = 0; i < n; i++) {
         int pivot = i;
-        for (int j = i + 1; j < n; j++)
+        for (int j = i; j < n; j++)
             if (A[j][i] != 0) {
                 pivot = j;
                 break;
@@ -50,6 +50,40 @@ vector<T> gauss(vector<vector<T>> A, vector<T> b) {
     return b;
 }
 
+template<class T>
+vector<vector<T>> inverse(vector<vector<T>> A) {
+    int n = A.size();
+    vector<vector<T>> B(n, vector<T>(n));
+    for (int i = 0; i < n; ++i)
+        B[i][i] = 1;
+    for (int i = 0; i < n; i++) {
+        int pivot = i;
+        for (int j = i; j < n; j++)
+            if (A[j][i] != 0) {
+                pivot = j;
+                break;
+            }
+        assert(A[pivot][i] != 0);
+        swap(A[i], A[pivot]);
+        swap(B[i], B[pivot]);
+        T Aii = A[i][i];
+        for (int j = 0; j < n; j++) {
+            A[i][j] /= Aii;
+            B[i][j] /= Aii;
+        }
+        for (int j = 0; j < n; j++) {
+            T Aji = A[j][i];
+            if (j != i && Aji != 0)
+                for (int k = 0; k < n; k++) {
+                    A[j][k] -= A[i][k] * Aji;
+                    B[j][k] -= B[i][k] * Aji;
+                }
+        }
+    }
+    return B;
+}
+
+#include "matrix.h"
 #include "../numbertheory/modint.h"
 
 constexpr int mod = (int) 1e9 + 7;
@@ -63,12 +97,9 @@ int main() {
                                  {-1, 3, 5}};
         vector<double> b = {1, 0, 0};
         vector<double> x = gauss(A, b);
-        for (int i = 0; i < A.size(); i++) {
-            double y = 0;
-            for (int j = 0; j < A[i].size(); j++)
-                y += A[i][j] * x[j];
-            assert(abs(b[i] - y) < 1e-9);
-        }
+        vector<vector<double >> y = A * transpose(vector<vector<double>>{x});
+        for (int i = 0; i < b.size(); i++)
+            assert(abs(b[i] - y[i][0]) < 1e-9);
     }
     {
         vector<vector<mint>> A{{4,  2, -1},
@@ -76,11 +107,13 @@ int main() {
                                {-1, 3, 5}};
         vector<mint> b = {1, 2, 3};
         vector<mint> x = gauss(A, b);
-        for (int i = 0; i < A.size(); i++) {
-            mint y = 0;
-            for (int j = 0; j < A[i].size(); j++)
-                y += A[i][j] * x[j];
-            assert(y == b[i]);
-        }
+        vector<vector<mint>> y = A * transpose(vector<vector<mint>>{x});
+        assert(transpose(y)[0] == b);
+
+        vector<vector<mint>> B = inverse(A);
+        vector<vector<mint>> E = A * B;
+        for (int i = 0; i < A.size(); ++i)
+            for (int j = 0; j < A.size(); ++j)
+                assert(E[i][j] == (i == j ? 1 : 0));
     }
 }
