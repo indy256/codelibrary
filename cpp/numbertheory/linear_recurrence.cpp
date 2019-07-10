abd77569590713e136393a1a407faf3a58f459cf
indy256 <indy256@gmail.com>
2019-07-10 03:18:56 +0300

    update

--- cpp/numbertheory/linear_recurrence.cpp
+++ cpp/numbertheory/linear_recurrence.cpp
@@ -16,40 +16,38 @@ T nth_element_of_recurrence(vector<T> a, const vector<T> &f, long long n) {
 }
 
 // https://en.wikipedia.org/wiki/Berlekamp%E2%80%93Massey_algorithm
-template<typename T>
-vector<T> berlekamp_massey(const vector<T> &a) {
-    vector<T> f = {1};
-    vector<T> p = {1};
+template<typename M>
+vector<M> berlekamp_massey(const vector<M> &a) {
+    int n = a.size();
+    vector<M> C(n), B(n);
+    C[0] = B[0] = 1;
+    M b = 1;
     int L = 0;
-    for (int i = 0; i < a.size(); i++) {
-        T delta = 0;
-        for (int j = 0; j <= L; j++) {
-            delta += a[i - j] * f[j];
+    for (int i = 0, m = 1; i < n; ++i) {
+        M d = a[i];
+        for (int j = 1; j <= L; ++j)
+            d = d + C[j] * a[i - j];
+        if (d == 0) {
+            ++m;
+            continue;
         }
-        p.insert(p.begin(), 0);
-        if (delta != 0) {
-            vector<T> t = f;
-            if (p.size() > t.size()) {
-                t.resize(p.size());
-            }
-            for (int j = 0; j < p.size(); j++) {
-                t[j] -= delta * p[j];
-            }
-            if (2 * L <= i) {
-                p = f;
-                T od = 1 / delta;
-                for (T &x : p) {
-                    x *= od;
-                }
-                L = i + 1 - L;
-            }
-            swap(f, t);
+        vector<M> T = C;
+        M coef = d / b;
+        for (int j = m; j < n; ++j)
+            C[j] -= coef * B[j - m];
+        if (2 * L > i) {
+            ++m;
+            continue;
         }
+        L = i + 1 - L;
+        B = T;
+        b = d;
+        m = 1;
     }
-    reverse(f.begin(), f.end());
-    assert(f.size() == L + 1);
-    f.erase(f.begin() + L);
-    return -f;
+    C.resize(L + 1);
+    C.erase(C.begin());
+    reverse(C.begin(), C.end());;
+    return -C;
 }
 
 // usage example
@@ -67,7 +65,7 @@ int main() {
         cout << endl;
     }
     {
-        vector<mint> f = berlekamp_massey(vector<mint>({1, 1, 2, 3, 5}));
+        vector<mint> f = berlekamp_massey(vector<mint>({1, 1, 3, 5, 11}));
         for (auto v:f) cout << (int) v << " ";
         cout << endl;
     }
