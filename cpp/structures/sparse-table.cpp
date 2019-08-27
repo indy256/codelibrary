26556af366c6c7fbb55b28ed2e98509db517057d
indy256 <indy256@gmail.com>
2019-08-27 16:52:11 +0300

    update

--- cpp/structures/sparse-table.cpp
+++ cpp/structures/sparse-table.cpp
@@ -9,21 +9,20 @@ using namespace std;
 
 template<class T, class F = function<T(const T &, const T &)>>
 struct SparseTable {
-    int n;
     vector<vector<T>> t;
     F func;
 
-    SparseTable(const vector<T> &a, F f) : n(a.size()), t(32 - _lzcnt_u32(n)), func(std::move(f)) {
+    SparseTable(const vector<T> &a, F f) : t(32 - _lzcnt_u32(a.size())), func(std::move(f)) {
         t[0] = a;
         for (size_t i = 1; i < t.size(); i++) {
-            t[i].resize(n - (1 << i) + 1);
+            t[i].resize(a.size() - (1 << i) + 1);
             for (size_t j = 0; j < t[i].size(); j++)
                 t[i][j] = func(t[i - 1][j], t[i - 1][j + (1 << (i - 1))]);
         }
     }
 
     T get(int from, int to) const {
-        assert(0 <= from && from <= to && to <= n - 1);
+        assert(0 <= from && from <= to && to <= (int) t[0].size() - 1);
         int k = 31 - _lzcnt_u32(to - from + 1);
         return func(t[k][from], t[k][to - (1 << k) + 1]);
     }
