42a59be283bbc10e5c0ef4120e6fee2edd7b0978
nhungbp <nhugn99.my@gmail.com>
2019-09-28 19:20:39 +0700

    Add MatrixTestByJunit

--- java/linearalgebra/Matrix.java
+++ java/linearalgebra/Matrix.java
@@ -1,6 +1,10 @@
 package linearalgebra;
 
+import org.junit.Test;
+
 import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 public class Matrix {
     public static int[][] matrixAdd(int[][] a, int[][] b) {
@@ -38,6 +42,37 @@ public class Matrix {
         } else {
             return matrixMul(a, matrixPow(a, p - 1));
         }
+    }
+
+    @Test
+    public void matrixPowTest1() {
+        int p = 0;
+        int[][] matrix = Matrix.matrixUnit(3);
+        int[][] a = new int[3][3];
+        assertArrayEquals(matrix, Matrix.matrixPow(a, p));
+    }
+
+    @Test
+    public void matrixPowTest2() {
+        int p = 1;
+        int[][] a = {{1, 2, 3}, {2, 3, 1}, {3, 1, 2}};
+        assertArrayEquals(a, Matrix.matrixPow(a, p));
+    }
+
+    @Test
+    public void matrixPowTest3() {
+        int p = 2;
+        int[][] a = {{1, 2}, {2, 3}};
+        int[][] matrix = {{5, 8}, {8, 13}};
+        assertArrayEquals(matrix, Matrix.matrixPow(a, p));
+    }
+
+    @Test
+    public void matrixPowTest4() {
+        int p = -1;
+        int[][] a = {{2, 3}, {3, 4}};
+        int[][] matrix = {{-4, 3}, {3, -2}};
+        assertArrayEquals(matrix, Matrix.matrixPow(a, p));
     }
 
     public static int[][] matrixSumPow(int[][] a, int p) {
