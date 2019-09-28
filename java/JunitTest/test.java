f0002d982ca3c44570afa07179f35bad056233db
Quang Phạm <nguyenhoangtuyen98@gmail.com>
2019-09-28 23:04:41 +0700

    add test 2

--- java/JunitTest/test.java
+++ java/JunitTest/test.java
@@ -11,47 +11,49 @@ class test {
 	@Test
 	void test1() {
 		Sort sort = new Sort();
-		
+		int[] a = {0, 1, 2, 3, 4, 5};
 		int[] b = {1, 4, 0, 3, 2, 5};
 		
 		int low = 0;
 		int high = 5;
 		sort.qSort(b, low, high);
+		assertEquals(a, b);
 	}
 	
 	@Test
 	void test2() {
 		Sort sort = new Sort();
-		
+		int[] a = {0, 1, 2, 3, 4, 5};
 		int[] b = {6, 4, 0, 3, 5, 2};
 		
 		int low = 0;
 		int high = 5;
 		sort.qSort(b, low, high);
-		
+		assertEquals(a, b);
 	}
 	
 	@Test
 	void test3() {
 		Sort sort = new Sort();
-		
+		int[] a = {0, 1, 2, 3, 4, 5};
 		int[] b = {1, 4, 0, 3, 5, 6};
 		
 		int low = 0;
 		int high = 5;
 		sort.qSort(b, low, high);
-		
+		assertEquals(a, b);
 	}
 	
 	@Test
 	void test4() {
 		Sort sort = new Sort();
-		
+		int[] a = {0, 1, 2, 3, 4, 5};
 		int[] b = {1, 4, 0, 3, 5, 2};
 		
 		int low = 3;
 		int high = 2;
 		sort.qSort(b, low, high);
+		assertEquals(a, b);
 	}
 
 }
