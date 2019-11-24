da6360ea3a6ff45d4e2b301b3d9b4fdbfde3dbe3
indy256 <indy256@gmail.com>
2019-11-24 10:54:56 +0300

    update

--- java/backtracking/MisWeighted.java
+++ java/backtracking/MisWeighted.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class MisWeighted {
 
-    // maximum independent weighted set
+    // maximum independent weighted set in O(3^(n/3))
+    // prerequisite: g[i] has i'th bit set
     public static int mis(long[] g, long unused, int[] weights) {
         if (unused == 0)
             return 0;
@@ -19,25 +20,6 @@ public class MisWeighted {
         return res;
     }
 
-    // maximum weighted clique with Bron-Kerbosch pruning
-    public static int maxClique(long[] g, long allowed, long forbidden, int[] weights) {
-        if (allowed == 0)
-            return 0;
-        int v = -1;
-        long all = allowed | forbidden;
-        for (int u = Long.numberOfTrailingZeros(all); u < g.length; u += Long.numberOfTrailingZeros(all >> (u + 1)) + 1)
-            if (v == -1 || Long.bitCount(allowed & ~g[v]) > Long.bitCount(allowed & ~g[u]))
-                v = u;
-        int res = 0;
-        long nv = allowed & ~g[v];
-        for (int u = Long.numberOfTrailingZeros(nv); u < g.length; u += Long.numberOfTrailingZeros(nv >> (u + 1)) + 1) {
-            res = Math.max(res, weights[u] + maxClique(g, allowed & g[u], forbidden & g[u], weights));
-            allowed ^= 1L << u;
-            forbidden |= 1L << u;
-        }
-        return res;
-    }
-
     // random test
     public static void main(String[] args) {
         Random rnd = new Random(1);
@@ -45,35 +27,18 @@ public class MisWeighted {
             int n = rnd.nextInt(16) + 1;
             long[] g = new long[n];
             int[] weights = new int[n];
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++)
                 weights[i] = rnd.nextInt(1000);
-                // for convenience of mis()
-                g[i] |= 1L << i;
-            }
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < i; j++)
                     if (rnd.nextBoolean()) {
                         g[i] |= 1L << j;
                         g[j] |= 1L << i;
                     }
+            for (int i = 0; i < n; i++)
+                g[i] |= 1 << i;
             int res1 = mis(g, (1L << n) - 1, weights);
             int res2 = misSlow(g, weights);
-            if (res1 != res2)
-                throw new RuntimeException();
-        }
-
-        for (int step = 0; step < 1000; step++) {
-            int n = rnd.nextInt(16) + 1;
-            long[] g = new long[n];
-            int[] weights = rnd.ints(n, 0, 1000).toArray();
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < i; j++)
-                    if (rnd.nextBoolean()) {
-                        g[i] |= 1L << j;
-                        g[j] |= 1L << i;
-                    }
-            int res1 = maxClique(g, (1L << n) - 1, 0, weights);
-            int res2 = maxCliqueSlow(g, weights);
             if (res1 != res2)
                 throw new RuntimeException();
         }
@@ -87,25 +52,6 @@ public class MisWeighted {
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < i; j++)
                     ok &= (set & (1 << i)) == 0 || (set & (1 << j)) == 0 || (g[i] & (1L << j)) == 0;
-            if (ok) {
-                int cur = 0;
-                for (int i = 0; i < n; i++)
-                    if ((set & (1 << i)) != 0)
-                        cur += weights[i];
-                res = Math.max(res, cur);
-            }
-        }
-        return res;
-    }
-
-    static int maxCliqueSlow(long[] g, int[] weights) {
-        int res = 0;
-        int n = g.length;
-        for (int set = 0; set < 1 << n; set++) {
-            boolean ok = true;
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < i; j++)
-                    ok &= (set & (1 << i)) == 0 || (set & (1 << j)) == 0 || (g[i] & (1 << j)) != 0;
             if (ok) {
                 int cur = 0;
                 for (int i = 0; i < n; i++)
