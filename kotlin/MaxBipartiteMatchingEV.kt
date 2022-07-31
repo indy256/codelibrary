3f4c45efb68a0d04dfe43f06ed2e6070e269d0c8
indy256 <indy256@gmail.com>
2022-07-31 09:31:24 -0400

    Use DeepRecursiveFunction in maxMatching

--- kotlin/MaxBipartiteMatchingEV.kt
+++ kotlin/MaxBipartiteMatchingEV.kt
@@ -1,20 +1,22 @@
 // https://en.wikipedia.org/wiki/Matching_(graph_theory)#In_unweighted_bipartite_graphs in O(V * E)
 fun maxMatching(graph: Array<out List<Int>>, n2: Int): Int {
     val n1 = graph.size
+    val vis = BooleanArray(n1)
     val matching = IntArray(n2) { -1 }
-    return (0 until n1).sumOf { findPath(graph, it, matching, BooleanArray(n1)) }
-}
 
-fun findPath(graph: Array<out List<Int>>, u1: Int, matching: IntArray, vis: BooleanArray): Int {
-    vis[u1] = true
-    for (v in graph[u1]) {
-        val u2 = matching[v]
-        if (u2 == -1 || !vis[u2] && findPath(graph, u2, matching, vis) == 1) {
-            matching[v] = u1
-            return 1
+    val findPath = DeepRecursiveFunction { u1 ->
+        vis[u1] = true
+        for (v in graph[u1]) {
+            val u2 = matching[v]
+            if (u2 == -1 || !vis[u2] && callRecursive(u2) == 1) {
+                matching[v] = u1
+                return@DeepRecursiveFunction 1
+            }
         }
+        0
     }
-    return 0
+
+    return (0 until n1).sumOf { vis.fill(false); findPath(it) }
 }
 
 // Usage example
